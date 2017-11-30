7ed59b3c4b3d15a3722210f6f2b21936349b96cf
Axel Siebenborn <axel.siebenborn@sap.com>
2017-11-30 09:06:17 +0100

    Initial population.

---
+++ build-pipe/build.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -ex
+
+if [ -d SapMachine ]; then
+    rm -rf SapMachine;
+fi
+
+git clone -b $SAPMACHINE_GIT_BRANCH "http://$GIT_USER:$GIT_PASSWORD@$SAPMACHINE_GIT_REPO" SapMachine
+
+cd SapMachine
+
+if [[ ! -z $GIT_TAG_NAME ]] && [[ $GIT_TAG_NAME == jdk-* ]]; then
+  git checkout $GIT_TAG_NAME
+  bash ./configure --with-boot-jdk=$BOOT_JDK --with-version-string=${GIT_TAG_NAME: 4} --with-version-opt="sapmachine"
+else
+  bash ./configure --with-boot-jdk=$BOOT_JDK --with-version-opt="sapmachine"
+fi
+
+make JOBS=12 images
+
+cd build
+cd "$(ls)"/images
+
+tar czf ../../../../"${SAPMACHINE_ARCHIVE_NAME_PREFIX}-jdk.tar.gz" jdk
+tar czf ../../../../"${SAPMACHINE_ARCHIVE_NAME_PREFIX}-jre.tar.gz" jre
