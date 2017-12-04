46713dccbb839351d57d57d90039234c26bcde1c
Rene Schuenemann <rene.schuenemann@sap.com>
2017-12-04 10:17:40 +0100

    Moved test preparation to dedicated shell script file.

---
+++ test-pipe/test_jtreg_prepare.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+if [ -d sapmachine ]; then
+    rm -rf sapmachine;
+fi
+
+git clone -b -b $SAPMACHINE_GIT_BRANCH "https://$SAPMACHINE_GIT_REPO" sapmachine
+
+if [[ ! -z $GIT_TAG_NAME ]] && [[ $GIT_TAG_NAME == jdk-* ]]; then
+  git checkout $GIT_TAG_NAME
+fi
