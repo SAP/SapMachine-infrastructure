3163308a58fe37d67f861d23ad2643b3790ba6ae
Rene Schuenemann <rene.schuenemann@sap.com>
2017-12-04 10:20:30 +0100

    Fixed git clone call.

--- test-pipe/test_jtreg_prepare.sh
+++ test-pipe/test_jtreg_prepare.sh
@@ -4,7 +4,7 @@ if [ -d sapmachine ]; then
     rm -rf sapmachine;
 fi
 
-git clone -b -b $SAPMACHINE_GIT_BRANCH "https://$SAPMACHINE_GIT_REPO" sapmachine
+git clone -b $SAPMACHINE_GIT_BRANCH "https://$SAPMACHINE_GIT_REPO" sapmachine
 
 if [[ ! -z $GIT_TAG_NAME ]] && [[ $GIT_TAG_NAME == jdk-* ]]; then
   git checkout $GIT_TAG_NAME
