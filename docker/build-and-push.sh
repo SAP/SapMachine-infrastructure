31ec85bc164d509860e4fa2de0397883643b7043
Axel Siebenborn <axel.siebenborn@sap.com>
2017-12-21 17:06:32 +0100

    Test docker image.

---
+++ docker/build-and-push.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -ex
+
+if [[ $1 == "jre" ]]; then
+  JRE=true
+else
+  JRE=false
+fi
+
+if $JRE ; then
+  docker build -t "$DOCKER_USER/jdk${VERSION_MAJOR}:${VERSION_MAJOR}.${VERSION_MINOR}-jre" \
+  -t "$DOCKER_USER/jdk${VERSION_MAJOR}:latest-jre"  "sapmachine-$VERSION_MAJOR/."
+else
+  docker build -t "$DOCKER_USER/jdk${VERSION_MAJOR}:${VERSION_MAJOR}.${VERSION_MINOR}" \
+  -t "$DOCKER_USER/jdk${VERSION_MAJOR}:latest"  "sapmachine-$VERSION_MAJOR/."
+fi
+docker login -u $DOCKER_USER -p $DOCKER_PASSWORD
+docker push "$DOCKER_USER/jdk${VERSION_MAJOR}"
