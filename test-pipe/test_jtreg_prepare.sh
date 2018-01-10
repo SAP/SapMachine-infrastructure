016dc40c816ef6bc58711ac474be306e77a62ac8
Axel Siebenborn <axel.siebenborn@sap.com>
2018-01-10 15:46:27 +0100

    JTREG tests - make build project configurable in order to copy the corresponding build artifact.

--- test-pipe/test_jtreg_prepare.sh
+++ test-pipe/test_jtreg_prepare.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 if [ -d sapmachine ]; then
     rm -rf sapmachine;
@@ -6,6 +6,6 @@ fi
 
 git clone -b $SAPMACHINE_GIT_BRANCH "https://$SAPMACHINE_GIT_REPO" sapmachine
 
-if [[ ! -z $GIT_TAG_NAME ]] && [[ $GIT_TAG_NAME == jdk-* ]]; then
+if [[ ! -z $GIT_TAG_NAME ]] && [[ $GIT_TAG_NAME == sapmachine* ]]; then
   git checkout $GIT_TAG_NAME
 fi
