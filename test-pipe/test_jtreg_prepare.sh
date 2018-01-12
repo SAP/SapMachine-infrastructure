a7f2ea3cfa9971098d1bbd635fb24554b1fba6ae
Axel Siebenborn <axel.siebenborn@sap.com>
2018-01-12 16:10:58 +0100

    Merge PR to have the correct test environment.

--- test-pipe/test_jtreg_prepare.sh
+++ test-pipe/test_jtreg_prepare.sh
@@ -6,6 +6,13 @@ fi
 
 git clone -b $SAPMACHINE_GIT_BRANCH "https://$SAPMACHINE_GIT_REPO" sapmachine
 
+cd sapmachine
+
 if [[ ! -z $GIT_TAG_NAME ]] && [[ $GIT_TAG_NAME == sapmachine* ]]; then
   git checkout $GIT_TAG_NAME
+fi
+
+if [ "$GITHUB_PR_NUMBER" ]; then
+  git fetch origin "pull/$GITHUB_PR_NUMBER/head"
+  git merge FETCH_HEAD
 fi
