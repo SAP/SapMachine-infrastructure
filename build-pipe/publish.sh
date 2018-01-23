e4966acbb43d81542ddfa330313e4db81e0cd3f8
Axel Siebenborn <axel.siebenborn@sap.com>
2018-01-23 13:18:51 +0100

    Fix publish script.

--- build-pipe/publish.sh
+++ build-pipe/publish.sh
@@ -21,10 +21,10 @@ if [ -z $GIT_TAG_NAME ]; then
         echo "Snapshot already published"
         exit 0
     fi
+    GIT_TAG_NAME="${SAPMACHINE_ARCHIVE_NAME_PREFIX}_snapshot-${TIMESTAMP}"
     git tag $GIT_TAG_NAME
     git push --tags
-    cd ..
-    GIT_TAG_NAME="${SAPMACHINE_ARCHIVE_NAME_PREFIX}_snapshot-${TIMESTAMP}"
+    popd
     GIT_TAG_DESCRIPTION="${SAPMACHINE_ARCHIVE_NAME_PREFIX} Snapshot ${TIMESTAMP_LONG}"
     github-release release -t $GIT_TAG_NAME --pre-release -d "$GIT_TAG_DESCRIPTION"
 
