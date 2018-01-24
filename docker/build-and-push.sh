813f258a1e04e4639ec49df09bb4e453a5c3baec
Rene Schuenemann <rene.schuenemann@sap.com>
2018-01-24 13:58:18 +0100

    Added support for Alpine Linux.

--- docker/build-and-push.sh
+++ docker/build-and-push.sh
@@ -7,6 +7,14 @@ else
   JRE=false
 fi
 
+if [[ $2 == "alpine" ]]; then
+  ALPINE=true
+  ALPINE_EXT="-alpine"
+else
+  ALPINE=false
+  ALPINE_EXT=
+fi
+
 REPO_URL="http://$GIT_USER:$GIT_PASSWORD@github.com/SAP/SapMachine-infrastructure/"
 
 if [ ! -d infra ]; then
@@ -23,11 +31,11 @@ docker images | grep sapmachine | awk '{print $3}' | xargs docker rmi -f
 set -e
 
 if $JRE ; then
-  docker build -t "$DOCKER_USER/jdk${VERSION_MAJOR}:${VERSION_MAJOR}.${VERSION_MINOR}.${SAPMACHINE_VERSION}-jre" \
-  -t "$DOCKER_USER/jdk${VERSION_MAJOR}:latest-jre"  "sapmachine-$VERSION_MAJOR-jre/."
+  docker build -t "$DOCKER_USER/jdk${VERSION_MAJOR}:${VERSION_MAJOR}.${VERSION_MINOR}.${SAPMACHINE_VERSION}-jre${ALPINE_EXT}" \
+  -t "$DOCKER_USER/jdk${VERSION_MAJOR}:latest-jre${ALPINE_EXT}"  "sapmachine-$VERSION_MAJOR-jre${ALPINE_EXT}/."
 else
-  docker build -t "$DOCKER_USER/jdk${VERSION_MAJOR}:${VERSION_MAJOR}.${VERSION_MINOR}.${SAPMACHINE_VERSION}" \
-  -t "$DOCKER_USER/jdk${VERSION_MAJOR}:latest"  "sapmachine-$VERSION_MAJOR/."
+  docker build -t "$DOCKER_USER/jdk${VERSION_MAJOR}:${VERSION_MAJOR}.${VERSION_MINOR}.${SAPMACHINE_VERSION}${ALPINE_EXT}" \
+  -t "$DOCKER_USER/jdk${VERSION_MAJOR}:latest${ALPINE_EXT}"  "sapmachine-$VERSION_MAJOR${ALPINE_EXT}/."
 fi
 docker login -u $DOCKER_USER -p $DOCKER_PASSWORD
 docker push "$DOCKER_USER/jdk${VERSION_MAJOR}"
