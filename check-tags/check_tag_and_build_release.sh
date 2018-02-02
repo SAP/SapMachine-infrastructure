e3b0a793daaa0fbab4d258d39d228c88f3cd5084
axel7born <axel.siebenborn@sap.com>
2018-02-02 16:34:23 +0100

    Check if tag already merged for all target branches.

--- check-tags/check_tag_and_build_release.sh
+++ check-tags/check_tag_and_build_release.sh
@@ -26,28 +26,26 @@ LAST_BUILD_JDK_TAG=$(git tag | sed -rn $REGEXP | sort -nr | head -n1)
 
 JDK_TAG="jdk-$MAJOR_VERSION+$LAST_BUILD_JDK_TAG"
 echo "LAST_JDK_TAG=$LAST_BUILD_JDK_TAG"
-set +e
-JDK_TAG_CONTAINING_BRANCH=$(git branch -a --contains tags/jdk-$MAJOR_VERSION+$LAST_BUILD_JDK_TAG 2> /dev/null | \
- grep -E $GREP_PATTERN )
 
-echo "$JDK_TAG_CONTAINING_BRANCH"
-SAPMACHINE_TAG_CONTAINING_BRANCH=$(git branch -a --contains tags/sapmachine-$MAJOR_VERSION+$LAST_BUILD_JDK_TAG-0 2> /dev/null | \
- grep -E $GREP_PATTERN )
-echo "$SAPMACHINE_TAG_CONTAINING_BRANCH"
+BRANCHES=( "$BASE_BRANCH" "$BASE_BRANCH-alpine" )
+for base in "${BRANCHES[@]}"
+do
+  GREP_PATTERN="($branch$)"
+  set +e
+  JDK_TAG_CONTAINING_BRANCH=$(git branch -a --contains tags/jdk-$MAJOR_VERSION+$LAST_BUILD_JDK_TAG 2> /dev/null | \
+  grep -E $GREP_PATTERN )
 
-set -e
+  echo "$JDK_TAG_CONTAINING_BRANCH"
+  SAPMACHINE_TAG_CONTAINING_BRANCH=$(git branch -a --contains tags/sapmachine-$MAJOR_VERSION+$LAST_BUILD_JDK_TAG-0 2> /dev/null | \
+  grep -E $GREP_PATTERN )
+  echo "$SAPMACHINE_TAG_CONTAINING_BRANCH"
+  set -e
 
-if [ "$JDK_TAG_CONTAINING_BRANCH" ] && [ -z "$SAPMACHINE_TAG_CONTAINING_BRANCH" ] ; then
-
-  BRANCHES=( "$BASE_BRANCH" "$BASE_BRANCH-alpine" )
-  for base in "${BRANCHES[@]}"
-  do
-	  echo $base
+  if [ "$JDK_TAG_CONTAINING_BRANCH" ] && [ -z "$SAPMACHINE_TAG_CONTAINING_BRANCH" ] ; then
     TAG_EXT=$(echo $base |  sed -rn "s/sapmachine[0-9]*(\-alpine)?/\1/p")
     SAPMACHINE_TAG="sapmachine-$MAJOR_VERSION+$LAST_BUILD_JDK_TAG-0$TAG_EXT"
     echo "Create tag $SAPMACHINE_TAG" 
     git checkout $base
-    
     git tag $SAPMACHINE_TAG
     git push --tags
 
@@ -55,5 +53,5 @@ if [ "$JDK_TAG_CONTAINING_BRANCH" ] && [ -z "$SAPMACHINE_TAG_CONTAINING_BRANCH" 
     CRUMB=$(curl --user $JENKINS_USER:$JENKINS_PASSWORD  $JENKINS_URL/crumbIssuer/api/xml?xpath=concat\(//crumbRequestField,%22:%22,//crumb\))
     curl -H $CRUMB -X POST --user $JENKINS_USER:$JENKINS_PASSWORD \
     "$JENKINS_URL/job/build-$MAJOR_VERSION-release$TAG_EXT/buildWithParameters?TOKEN=test-token&GIT_TAG_NAME=$URL_TAG&PUBLISH=false&RUN_TESTS=false"
-  done
-fi
+  fi
+done
