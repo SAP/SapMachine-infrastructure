f56c9ae934fae4e842a6412b1a07ed15bf81f9e3
Rene Schuenemann <rene.schuenemann@sap.com>
2018-03-19 11:20:37 +0100

    Migrate to new Docker CE version.

--- cloud-infrastructure/ci-slave-local/start-slave.sh
+++ cloud-infrastructure/ci-slave-local/start-slave.sh
@@ -4,4 +4,10 @@ MASTER_URL=$1
 SECRET=$2
 SLAVE_NO_CERTIFICATE_CHECK=$3
 
+JAVA_ARGS="-Xms10g"
+
+if [-f nohup.out ]; then
+    cp nohup.out >> "slave-$(date +"%d_%m_%y-%H_%M_%S_%N").out"
+fi
+
 nohup java -jar /home/jenkins/slave.jar -jnlpUrl ${MASTER_URL} -secret ${SECRET} -workDir /home/jenkins/slave-home ${SLAVE_NO_CERTIFICATE_CHECK} &
