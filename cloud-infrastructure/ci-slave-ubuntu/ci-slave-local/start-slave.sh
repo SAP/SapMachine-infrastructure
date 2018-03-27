d18f36b4fd515f281764a839e0f9882a2629cb9c
Rene Schuenemann <rene.schuenemann@sap.com>
2018-03-27 10:35:25 +0200

    Added more tools to the local ubuntu slave.

--- cloud-infrastructure/ci-slave-ubuntu/ci-slave-local/start-slave.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-MASTER_URL=$1
-SECRET=$2
-SLAVE_NO_CERTIFICATE_CHECK=$3
-
-nohup java -jar /home/jenkins/slave.jar -jnlpUrl ${MASTER_URL} -secret ${SECRET} -workDir /home/jenkins/slave-home ${SLAVE_NO_CERTIFICATE_CHECK} &
