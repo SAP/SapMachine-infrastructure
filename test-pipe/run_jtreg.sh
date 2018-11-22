11de46683bdef4fa705b763967141bc800c5ae6d
Arno Zeller <36733853+ArnoZeller@users.noreply.github.com>
2018-11-22 10:18:49 +0100

    Update run_jtreg.sh
    Remove -agentvm param from jterg for jdk test to avoid pooling of VMs. The pool grows up to 20 VMs and lead to native OOM errors.

--- test-pipe/run_jtreg.sh
+++ test-pipe/run_jtreg.sh
@@ -73,7 +73,7 @@ if [ "${TEST_SUITE}" == "jdk" ]; then
     ${JT_HOME}/bin/jtreg -dir:${JDK_LOCATION}/test/${TEST_SUITE} -xml -verbose:summary -nativepath:${TEST_NATIVE_LIB} \
     -exclude:${JDK_LOCATION}/test/${TEST_SUITE}/ProblemList.txt -exclude:${JDK_LOCATION}/test/${TEST_SUITE}/ProblemList-SapMachine.txt  \
     -conc:${CONCURRENCY} -vmoption:-XX:MaxRAMPercentage=${MAX_RAM_PERCENTAGE} \
-    -a -ignore:quiet -timeoutFactor:5 -agentvm -javaoption:-Djava.awt.headless=true "-k:(!headful)&(!printer)" -testjdk:${TEST_JDK} ${TEST_GROUPS}
+    -a -ignore:quiet -timeoutFactor:5 -javaoption:-Djava.awt.headless=true "-k:(!headful)&(!printer)" -testjdk:${TEST_JDK} ${TEST_GROUPS}
 fi
 
 if [ "${TEST_SUITE}" == "langtools" ]; then
