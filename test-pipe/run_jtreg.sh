420689e1541abdea864af6064ec84ab469d6998f
Arno Zeller <arno.zeller@sap.com>
2018-12-14 12:39:05 +0100

    use -w and -r to set report- and work-directory for jtreg tests. This allows to no longer need to move directories after the test run

--- test-pipe/run_jtreg.sh
+++ test-pipe/run_jtreg.sh
@@ -61,18 +61,20 @@ export PATH=${TEST_JDK}:$PATH
 export JT_JAVA=${TEST_JDK}
 export JAVA_HOME=${TEST_JDK}
 
+mkdir test_report_${TEST_SUITE}
+
 # Use full concurrency and agentvm to let the tests pass as fast as possible.
 if [ "${TEST_SUITE}" == "hotspot" ]; then
     ${JT_HOME}/bin/jtreg -dir:${JDK_LOCATION}/test/${TEST_SUITE}/jtreg -xml -verbose:summary -nativepath:${TEST_NATIVE_LIB} \
      -exclude:${JDK_LOCATION}/test/${TEST_SUITE}/jtreg/ProblemList.txt -exclude:${JDK_LOCATION}/test/${TEST_SUITE}/jtreg/ProblemList-SapMachine.txt \
-     -conc:${NUM_CPUS} -vmoption:-Xmx384m \
+     -conc:${NUM_CPUS} -vmoption:-Xmx384m -w:test_report_${TEST_SUITE}\JTWork -r:test_report_${TEST_SUITE}\JTreport \
      -a -ignore:quiet -timeoutFactor:5 -agentvm -javaoption:-Djava.awt.headless=true -javaoption:-DSCIMARK_2_CP=${SCIMARK_2_CP} "-k:(!ignore)&(!stress)&(!headful)" -testjdk:${TEST_JDK} ${TEST_GROUPS}
 fi
 
 if [ "${TEST_SUITE}" == "jdk" ]; then
     ${JT_HOME}/bin/jtreg -dir:${JDK_LOCATION}/test/${TEST_SUITE} -xml -verbose:summary -nativepath:${TEST_NATIVE_LIB} \
     -exclude:${JDK_LOCATION}/test/${TEST_SUITE}/ProblemList.txt -exclude:${JDK_LOCATION}/test/${TEST_SUITE}/ProblemList-SapMachine.txt  \
-    -conc:${NUM_CPUS} -vmoption:-Xmx384m \
+    -conc:${NUM_CPUS} -vmoption:-Xmx384m -w:test_report_${TEST_SUITE}\JTWork -r:test_report_${TEST_SUITE}\JTreport \
     -a -ignore:quiet -timeoutFactor:5 -agentvm -javaoption:-Djava.awt.headless=true "-k:(!headful)&(!printer)" -testjdk:${TEST_JDK} ${TEST_GROUPS}
 fi
 
@@ -80,6 +82,6 @@ fi
 if [ "${TEST_SUITE}" == "langtools" ]; then
     ${JT_HOME}/bin/jtreg -dir:${JDK_LOCATION}/test/${TEST_SUITE} -xml -verbose:summary \
     -exclude:${JDK_LOCATION}/test/${TEST_SUITE}/ProblemList.txt -exclude:${JDK_LOCATION}/test/${TEST_SUITE}/ProblemList-SapMachine.txt \
-    -conc:${CONCURRENCY} -vmoption:-Xmx640m \
+    -conc:${CONCURRENCY} -vmoption:-Xmx640m -w:test_report_${TEST_SUITE}\JTWork -r:test_report_${TEST_SUITE}\JTreport \
     -a -ignore:quiet -timeoutFactor:5 -agentvm -testjdk:${TEST_JDK} ${TEST_GROUPS}
 fi
