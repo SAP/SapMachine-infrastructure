02ce959e813ece089e8be593acb879c1ded0a9b0
Andreas Knoll <knolly@users.noreply.github.com>
2019-01-28 18:05:54 +0100

    added logic to get the path to the SapMachine
    it was not easy to geht the path from outside the script, so I added it into the python script

--- lib/run_dacapo.py
+++ lib/run_dacapo.py
@@ -3,25 +3,30 @@ Copyright (c) 2001-2018 by SAP SE, Walldorf, Germany.
 All rights reserved. Confidential and proprietary.
 '''
 
+import os
 import sys
 import time
 import subprocess
+from pathlib import Path 
 
-JAR        = "/opt/dacapo/dacapo.jar"
-HEADL      = "-Djava.awt.headless=true"
-PARA       = "--max-iterations=35 --variance=5 --verbose"
-timeout    = 600
+JAR         = "dacapo-9.12-MR1-bach.jar"
+HEADL       = "-Djava.awt.headless=true"
+PARA        = "--max-iterations=35 --variance=5 --verbose"
+timeout     = 450
 
-#
-# check the argument (java path)
-#
+# get path to the SapMachine
+CURRENT_DIR = os.getcwd()
+os.chdir('SapMachine')
+os.chdir('build')
+ARR = os.listdir()
+BUILD_TYPE  = ARR[0]
+JAV = CURRENT_DIR + "/SapMachine/build/" + BUILD_TYPE + "/images/jdk/bin/java"
 
-if len(sys.argv) != 2:
-  print('missing java path. Syntax: python3' + str(sys.argv[0]) + ' <path to java>')
-  sys.exit(-1)
-
-JAV = sys.argv[1]
+# print Java Version
 subprocess.call([ JAV , '-version'])
+
+#clean up 
+os.chdir(CURRENT_DIR)
 
 
 #
@@ -42,7 +47,7 @@ def timerfunc(func):
           print('Testrun OK')
           return 0
         print('ERROR: Test did run in timeout')
-        return sys.exit(-2)
+        return diff
 
     return function_timer
 
@@ -54,14 +59,14 @@ def timerfunc(func):
 @timerfunc
 def call_dacapo():
 
+   # default run:
    result =  subprocess.call([ JAV ,HEADL,'-jar',JAR,'--max-iterations=35','--variance=5','--verbose','--converge','fop','avrora','h2','luindex','lusearch','pmd','xalan'])
 
    if result != 0:
      print('ERROR: Test did run in error: ', result)
-     return sys.exit(-3)
+     return result
 
-
+	 
 if __name__ == '__main__':
     call_dacapo()
      
-
