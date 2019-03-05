2163feddbd6d449d11302519a68c1487b76c961f
Rene Schuenemann <rene.schuenemann@gmail.com>
2019-03-05 14:43:21 +0100

    Adde script to list all available OepnJDK repositories.

---
+++ lib/list_openjdk_repositories.py
@@ -0,0 +1,51 @@
+'''
+Copyright (c) 2001-2019 by SAP SE, Walldorf, Germany.
+All rights reserved. Confidential and proprietary.
+'''
+
+import os
+import sys
+import json
+import re
+import utils
+import argparse
+
+from urllib2 import urlopen, Request, quote, HTTPError
+
+openjdk_hg_base = 'http://hg.openjdk.java.net/'
+
+def test_repositories(repository_base, repository_suffix=''):
+    openjdk_repositories = ['jdk/jdk']
+    code = 200
+    jdk_major = 10
+
+    while code == 200:
+        repository = repository_base + str(jdk_major) + repository_suffix
+        jdk_major += 1
+
+        request = Request(openjdk_hg_base + repository)
+
+        try:
+            response = urlopen(request)
+            code = response.code
+            openjdk_repositories.append(repository)
+        except HTTPError, e:
+            code = e.code
+
+    return openjdk_repositories
+
+def main(argv=None):
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-s', '--separator', help='the separator char', metavar='SEPARATOR', required=False, default=' ')
+    args = parser.parse_args()
+
+    openjdk_repositories = []
+
+    openjdk_repositories.extend(test_repositories('jdk/jdk'))
+    openjdk_repositories.extend(test_repositories('jdk-updates/jdk', 'u'))
+
+    print(args.separator.join(openjdk_repositories))
+    return 0
+
+if __name__ == "__main__":
+    sys.exit(main())
