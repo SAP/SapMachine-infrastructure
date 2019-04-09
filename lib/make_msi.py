0f6acb0ae76d96f1127501fa0966fae0f6bbaec3
Rene Schuenemann <rene.schuenemann@gmail.com>
2019-04-09 16:24:12 +0200

    Use full version for msi name.

--- lib/make_msi.py
+++ lib/make_msi.py
@@ -137,7 +137,7 @@ def main(argv=None):
     utils.run_cmd('candle -arch x64 SapMachine.wxs'.split(' '), cwd=work_dir)
     utils.run_cmd('light -ext WixUIExtension SapMachine.wixobj'.split(' '), cwd=work_dir)
 
-    os.rename(join(work_dir, 'SapMachine.msi'), join(cwd, str.format('sapmachine-jdk-{0}_windows-x64_bin.msi', version_part)))
+    os.rename(join(work_dir, 'SapMachine.msi'), join(cwd, str.format('sapmachine-jdk-{0}_windows-x64_bin.msi', version)))
 
     return 0
 
