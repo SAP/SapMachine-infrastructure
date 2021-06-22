5817f1c079c307e919f7b498e57ffd575f82bdd0
Arno Zeller <arno.zeller@sap.com>
2021-06-22 11:28:22 +0200

    Create download_jtreg.py
    Add script to download and extract jtreg

---
+++ lib/download_jtreg.py
@@ -0,0 +1,38 @@
+'''
+Copyright (c) 2001-2021 by SAP SE, Walldorf, Germany.
+All rights reserved. Confidential and proprietary.
+'''
+
+from argparse import ArgumentParser
+import sys
+import utils
+import os
+from os.path import join
+
+def main(argv=None):
+    parser = ArgumentParser()
+    parser.add_argument('-m', '--major', help='the SapMachine major version to build', metavar='MAJOR', required=True)
+    parser.add_argument('-d', '--dir', help='The dir to extract jtreg to', metavar='DIR', required=True)
+    args = parser.parse_args()
+
+    ver = int(args.major)
+    if ver >= 17:
+        url = 'https://ci.sapmachine.io/view/Infrastructure/job/jtreg/lastSuccessfulBuild/artifact/jtreg.zip'
+        name = 'jtreg.zip'
+    else:
+        url = 'https://github.com/SAP/SapMachine-infrastructure/releases/download/jtreg5.2/jtreg5.2.zip'
+        name = 'jtreg5.2.zip'
+
+    print(str.format('Downloading "{0}" and extracting to "{1}"', url, args.dir ))
+
+    archive_path = join(args.dir, name)
+    utils.remove_if_exists(archive_path)
+    utils.download_artifact(url, archive_path)
+    path = join(args.dir, 'jtreg')
+    utils.remove_if_exists(path)
+    os.makedirs(path)
+    utils.extract_archive(archive_path, path)    
+
+
+if __name__ == "__main__":
+    sys.exit(main())
