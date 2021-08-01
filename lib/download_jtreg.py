7748f68adc2c24fd94e32655a917f62b4ddbe236
Christoph Langer <christoph.langer@sap.com>
2021-08-01 23:16:11 +0200

    Some cleanup to download_jtreg.py

--- lib/download_jtreg.py
+++ lib/download_jtreg.py
@@ -12,21 +12,19 @@ from zipfile import ZipFile
 
 def main(argv=None):
     parser = ArgumentParser()
-    parser.add_argument('-m', '--major', help='the SapMachine major version to build', metavar='MAJOR', required=True)
+    parser.add_argument('-m', '--major', help='The SapMachine major version to build', metavar='MAJOR', required=True)
     parser.add_argument('-d', '--dir', help='The dir to extract jtreg to', metavar='DIR', required=True)
     args = parser.parse_args()
 
     ver = int(args.major)
     if ver >= 17:
         url = 'https://github.com/SAP/SapMachine-infrastructure/releases/download/jtreg-6/jtreg.zip'
-        name = 'jtreg.zip'
     else:
         url = 'https://github.com/SAP/SapMachine-infrastructure/releases/download/jtreg-5.1/jtreg.zip'
-        name = 'jtreg.zip'
 
     print(str.format('Downloading "{0}" and extracting to "{1}"', url, args.dir ))
 
-    archive_path = join(args.dir, name)
+    archive_path = join(args.dir, 'jtreg.zip')
     utils.remove_if_exists(archive_path)
     utils.download_artifact(url, archive_path)
     path = join(args.dir, 'jtreg')
