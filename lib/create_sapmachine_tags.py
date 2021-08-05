d20536ee14021241176753b22919172074f10ca5
Christoph Langer <christoph.langer@sap.com>
2021-08-05 13:28:50 +0200

    Disable linux_ppc64 builds. We stopped supporting the platofrm now after July 2021 update releases of SapMachine 11 and 16. Full removal of this platform from the source code shall be done later, e.g. after October 2021

--- lib/create_sapmachine_tags.py
+++ lib/create_sapmachine_tags.py
@@ -47,8 +47,8 @@ def run_jenkins_jobs(major, tag):
         str.format('build-{0}-release-windows_x86_64', major)
     ]
 
-    if major < 17:
-        build_jobs.append(str.format('build-{0}-release-linux_ppc64', major))
+#    if major < 17:
+#        build_jobs.append(str.format('build-{0}-release-linux_ppc64', major))
 
     if major > 16:
         build_jobs.append(str.format('build-{0}-release-macos_aarch64', major))
