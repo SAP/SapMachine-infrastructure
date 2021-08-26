bca4f8a8ce4c814ce36280c272eaac6a7e754a97
Gunter Haug <42777423+ghaug@users.noreply.github.com>
2021-08-26 16:13:49 +0200

    Disable brew casks

--- lib/publish.sh
+++ lib/publish.sh
@@ -90,6 +90,7 @@ if [ $UNAME == Darwin ]; then
     JDK_SHA256=`shasum -a 256 $DMG_NAME_JDK | awk '{ print $1 }'`
     JRE_SHA256=`shasum -a 256 $DMG_NAME_JRE | awk '{ print $1 }'`
 
-    python3 SapMachine-Infrastructure/lib/make_cask.py -t $GIT_TAG_NAME --sha256sum $JDK_SHA256 -i jdk $PRE_RELEASE_OPT
-    python3 SapMachine-Infrastructure/lib/make_cask.py -t $GIT_TAG_NAME --sha256sum $JRE_SHA256 -i jre $PRE_RELEASE_OPT
+    # Don't make the brew casks for the moment, we'll have to find a sound solution for that
+    #python3 SapMachine-Infrastructure/lib/make_cask.py -t $GIT_TAG_NAME --sha256sum $JDK_SHA256 -i jdk $PRE_RELEASE_OPT
+    #python3 SapMachine-Infrastructure/lib/make_cask.py -t $GIT_TAG_NAME --sha256sum $JRE_SHA256 -i jre $PRE_RELEASE_OPT
 fi
