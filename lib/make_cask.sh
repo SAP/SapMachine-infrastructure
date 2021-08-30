4863d5721952a96781585620b138c2c124f2f29d
Gunter Haug <42777423+ghaug@users.noreply.github.com>
2021-08-30 15:18:03 +0200

    add debug code

--- lib/make_cask.sh
+++ lib/make_cask.sh
@@ -2,3 +2,14 @@
 set -ex
 
 echo 'will check for casks here...'
+
+TIMESTAMP=`date +'%Y%m%d_%H_%M_%S'`
+TIMESTAMP_LONG=`date +'%Y/%m/%d %H:%M:%S'`
+
+export GITHUB_API_ACCESS_TOKEN=$SAPMACHINE_PUBLISH_GITHUB_TOKEN
+
+echo 'will check for casks here...'
+echo "GITHUB_API_ACCESS_TOKEN: $GITHUB_API_ACCESS_TOKEN"
+echo "SAPMACHINE_GIT_REPOSITORY: $SAPMACHINE_GIT_REPOSITORY"
+
+ls -la
