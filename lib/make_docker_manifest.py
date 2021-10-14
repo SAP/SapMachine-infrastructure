c1949bbf3bcc73c1eb2271abff32889f27acaa5a
Gunter Haug <gunter.haug@sap.com>
2021-10-14 15:13:35 +0200

    Make make_docker_manifest.py multi LTS capable

--- lib/make_docker_manifest.py
+++ lib/make_docker_manifest.py
@@ -13,7 +13,7 @@ import argparse
 from os.path import join
 from string import Template
 
-template_manifest = '''Maintainers: Rene Schuenemann <sapmachine@sap.com> (@reshnm)
+template_manifest = '''Maintainers: The SapMachine Team <sapmachine@sap.com> (@sapmachine)
 GitRepo: https://github.com/SAP/SapMachine-infrastructure.git
 
 ${images}
@@ -36,24 +36,38 @@ def main(argv=None):
     manifest = args.manifest
 
     images = ''
+    latest_lts = '00'
+    latest = '00'
+    releases = []
 
     for root, dirs, files in os.walk(dockerfiles_dir, topdown=False):
         for dir in dirs:
-            if dir == 'stable' or dir == 'lts':
-                directory = join(root, dir)
+            if re.match(r"\d\d\d?", dir):
+                releases.append(dir)
+                if int(dir) > int(latest):
+                    latest = dir
+                if utils.sapmachine_is_lts(dir) and int(dir) > int(latest_lts):
+                    latest_lts = dir
 
-                with open(join(directory, 'Dockerfile'), 'r') as dockerfile:
-                    version_match = dockerfile_version_pattern.search(dockerfile.read())
+    for release in releases:
+       print(release)
+       directory = join(root, release)
+       with open(join(directory, 'Dockerfile'), 'r') as dockerfile:
+           version_match = dockerfile_version_pattern.search(dockerfile.read())
 
-                major = version_match.group(1)
-                version = version_match.group(2)
-                retcode, git_commit, err = utils.run_cmd(['git', 'log', '-n', '1', '--pretty=format:%H', '--', join(directory, 'Dockerfile')], std=True)
-                tags = str.format('{0},{1} {2}', major, str.format(' {0},', version) if version != major else '', 'latest' if dir == 'stable' else 'lts')
+           major = version_match.group(1)
+           version = version_match.group(2)
+           retcode, git_commit, err = utils.run_cmd(['git', 'log', '-n', '1', '--pretty=format:%H', '--', join(directory, 'Dockerfile')], std=True)
+           tags = major
+           if release == latest:
+               tags += ', latest'
+           if release == latest_lts:
+               tags += ', lts'
 
-                if images:
-                    images += '\n\n'
+           if images:
+               images += '\n\n'
 
-                images += Template(template_image).substitute(tags=tags, git_commit=git_commit, directory=directory)
+           images += Template(template_image).substitute(tags=tags, git_commit=git_commit, directory=directory)
 
     with open(manifest, 'w') as manifest_file:
         manifest_file.write(Template(template_manifest).substitute(images=images))
