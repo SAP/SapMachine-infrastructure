4fce1615105b71f46b42c345465e7e270c17bb79
Christoph Langer <christoph.langer@sap.com>
2021-11-12 00:42:58 +0100

    Improve ansible setup of ppc64le build clients

--- cloud-infrastructure/ci-client-local/start-client.sh
+++ cloud-infrastructure/ci-client-local/start-client.sh
@@ -4,10 +4,10 @@ SERVER_URL=$1
 SECRET=$2
 CLIENT_NO_CERTIFICATE_CHECK=$3
 
-JAVA_ARGS="-Xms10g"
+JAVA_ARGS="-Xms4g"
 
 if [ -f nohup.out ]; then
     mv nohup.out "client-$(date +"%d_%m_%y-%H_%M_%S_%N").out"
 fi
 
-nohup java -jar /home/jenkins/client.jar -jnlpUrl ${SERVER_URL} -secret ${SECRET} -workDir /home/jenkins/client-home ${CLIENT_NO_CERTIFICATE_CHECK} &
+nohup ./sapmachine-jre/bin/java -jar /home/jenkins/client.jar -jnlpUrl ${SERVER_URL} -secret ${SECRET} -workDir /home/jenkins/client-home ${CLIENT_NO_CERTIFICATE_CHECK} > nohup.out 2>&1 &
