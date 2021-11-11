da097242e655b4530ccdecbdf9b43a54d14d9bf4
Gunter Haug <gunter.haug@sap.com>
2021-11-11 14:09:11 +0100

    build_asyncprof.sh

--- lib/build_asyncprof.sh
+++ lib/build_asyncprof.sh
@@ -1,11 +1,25 @@
 #!/bin/bash
 set -ex
 
+UNAME=`uname`
+
 if [[ -z $WORKSPACE ]]; then
   WORKSPACE=$PWD
 fi
 
+if [[ ! -z $GIT_TAG_NAME ]]; then
+  VERSION=${GIT_TAG_NAME:1:3}
+else
+  VERSION=snapshot
+fi
+
+if [[ $JOB_NAME =~ .*linux_ppc64l.e* ]]; then PLATFORM="linux-ppc64le" ; fi
+if [[ $JOB_NAME =~ .*linux_x86_64.* ]]; then PLATFORM="linux-x64" ; fi
+if [[ $JOB_NAME =~ .*macos.* ]]; then PLATFORM="macos" ; fi
+
 cd "${WORKSPACE}/async-profiler"
+
+TARGET_DIR="async-profiler-${VERSION}-${PLATFORM}"
 
 git config user.name SAPMACHINE_GIT_USER
 git config user.email SAPMACHINE_GIT_EMAIL
@@ -26,4 +40,13 @@ fi
 
 export JAVA_HOME=${BUILD_JDK}
 
-make
+make all
+
+rm -rf ${TARGET_DIR} ${TARGET_DIR}.tar.gz ${TARGET_DIR}.zip
+mkdir ${TARGET_DIR}
+cp -r build CHANGELOG.md LICENSE profiler.sh README.md ${TARGET_DIR}
+if [[ $UNAME == "Linux" ]]; then
+  tar czf ${TARGET_DIR}.tar.gz ${TARGET_DIR}
+else
+  zip -r ${TARGET_DIR}.zip ${TARGET_DIR}
+fi
