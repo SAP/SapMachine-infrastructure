e746f44b53e61382bbb0e2ff9ea22c7121633a09
Gunter Haug <42777423+ghaug@users.noreply.github.com>
2021-11-12 10:11:20 +0100

    Update test_asyncprof.sh

--- lib/test_asyncprof.sh
+++ lib/test_asyncprof.sh
@@ -11,9 +11,4 @@ cd "${WORKSPACE}/async-profiler"
 
 export JAVA_HOME=${BUILD_JDK}
 
-if [[ $UNAME == Linux ]]; then
-  sudo sysctl kernel.perf_event_paranoid=1
-  sudo sysctl kernel.kptr_restrict=0
-fi
-
 make test
