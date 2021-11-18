826b70aa79e627a3b6858599f95273bc7c35241e
Gunter Haug <gunter.haug@sap.com>
2021-11-18 15:57:00 +0100

    merge_async_prof.sh

--- lib/merge_async_prof.sh
+++ lib/merge_async_prof.sh
@@ -10,7 +10,8 @@ cd "${WORKSPACE}/async-profiler"
 git config user.name SAPMACHINE_GIT_USER
 git config user.email SAPMACHINE_GIT_EMAIL
 
-git pull
+git checkout master
+git pull 
 git remote add upstream https://github.com/jvm-profiling-tools/async-profiler.git
 git fetch upstream
 git checkout master
