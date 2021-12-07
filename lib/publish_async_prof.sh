97b49d337378585922e761a9cb862b37c77ee965
Gunter Haug <gunter.haug@sap.com>
2021-12-07 09:39:15 +0100

    publish_async_prof.sh build_asyncprof.sh

--- lib/publish_async_prof.sh
+++ lib/publish_async_prof.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -ex
 
+cd "${WORKSPACE}/async-profiler"
+
 ARCHIVE_NAME="$(cat artifact.txt)"
 
 python3 SapMachine-Infrastructure/lib/github_publish_asyncprof.py  -t $GIT_TAG_NAME -a "${ARCHIVE_NAME}"
