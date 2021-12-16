61fc1402e6789b421f8dd32426ddb77290f028ec
Christoph Langer <christoph.langer@sap.com>
2021-12-16 14:23:45 +0100

    Always download and extract devkit Storing it in a central location doesn't work

--- lib/deploy-devkit.sh
+++ lib/deploy-devkit.sh
@@ -5,18 +5,15 @@ NEXUS_PATH=https://common.repositories.cloud.sap/artifactory/sapmachine-mvn/sapm
 DEVKIT_VERSION=13.1
 DEVKIT_BASENAME=$1
 
-DEVKIT_PATH_UNRESOLVED=`pwd`/../${DEVKIT_BASENAME}
-
-if [ -d $DEVKIT_PATH_UNRESOLVED ]; then
-  DEVKIT_PATH=$(cd $DEVKIT_PATH_UNRESOLVED ; pwd)
-  echo $DEVKIT_PATH already exists
+if [ -d ${DEVKIT_BASENAME} ]; then
+  echo ${DEVKIT_BASENAME} already exists
   exit 0
 fi
 
-mkdir $DEVKIT_PATH_UNRESOLVED
-pushd $DEVKIT_PATH_UNRESOLVED
+mkdir ${DEVKIT_BASENAME}
+pushd ${DEVKIT_BASENAME}
 curl -s -u ${ART_USER}:${ART_PASSWORD} ${NEXUS_PATH}/${DEVKIT_VERSION}/${DEVKIT_BASENAME}.tar.tgz --output ${DEVKIT_BASENAME}.tgz
 tar xzf ${DEVKIT_BASENAME}.tgz
 rm ${DEVKIT_BASENAME}.tgz
-echo extracted devkit to `pwd`
 popd
+echo extracted devkit to ${DEVKIT_BASENAME}
