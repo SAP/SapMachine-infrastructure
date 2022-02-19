f2e7d6ce2241f4b27f815b71d111b6dd60e98ec6
Christoph Langer <christoph.langer@sap.com>
2022-02-19 18:48:32 +0100

    Fix copy/paste error

--- lib/ossrh/upload.sh
+++ lib/ossrh/upload.sh
@@ -9,6 +9,6 @@ mkdir upload
 cd upload
 cp ../SapMachine-Infrastructure/lib/ossrh/upload_pom.xml .
 mvn -B --no-transfer-progress --settings $OSSRH_SETTINGS_XML -f upload_pom.xml -Dtype=jre -Dversion=$VERSION -DartefactSetVersion=$MAJOR_VERSION clean deploy
-mvn -B --no-transfer-progress --settings $OSSRH_SETTINGS_XML -f upload_pom.xml -Dtype=jdk -Dversion=$VERSION -DartefactSetVersion=$MAJOR_VERSION | cut -d . -f 1` clean deploy
+mvn -B --no-transfer-progress --settings $OSSRH_SETTINGS_XML -f upload_pom.xml -Dtype=jdk -Dversion=$VERSION -DartefactSetVersion=$MAJOR_VERSION clean deploy
 
 rm -rf ~/.gnupg/* $GPGSEC $OSSRH_SETTINGS_XML
