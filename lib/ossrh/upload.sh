b6a910a9c7d6ab617463bf652c1320f09e019b82
Christoph Langer <christoph.langer@sap.com>
2022-02-20 00:44:12 +0100

    Improve variables in OSSRH upload

--- lib/ossrh/upload.sh
+++ lib/ossrh/upload.sh
@@ -7,9 +7,33 @@ gpg --import $GPGSEC
 
 mkdir upload
 cd upload
-cp ../SapMachine-Infrastructure/lib/ossrh/upload_pom.xml .
+
 mvn --version
-mvn -B --no-transfer-progress --settings $OSSRH_SETTINGS_XML -f upload_pom.xml -Dtype=jre -Dversion=$VERSION -DartefactSetVersion=$MAJOR_VERSION clean deploy
-mvn -B --no-transfer-progress --settings $OSSRH_SETTINGS_XML -f upload_pom.xml -Dtype=jdk -Dversion=$VERSION -DartefactSetVersion=$MAJOR_VERSION clean deploy
+
+cp ../SapMachine-Infrastructure/lib/ossrh/upload_pom.xml .
+sed -i "s/\${type}/jre/g" upload_pom.xml
+sed -i "s/\${version}/$VERSION/g" upload_pom.xml
+sed -i "s/\${maven.name}/SapMachine JRE/g" upload_pom.xml
+if [[ $MAJOR_VERSION == 11 ]]; then
+    sed -i "s/\${macosx.platform.name}/osx/g" upload_pom.xml
+    sed -i "s/\${skip.macaarch}/true/g" upload_pom.xml
+else
+    sed -i "s/\${macosx.platform.name}/macos/g" upload_pom.xml
+    sed -i "s/\${skip.macaarch}/false/g" upload_pom.xml
+fi
+mvn -B --no-transfer-progress --settings $OSSRH_SETTINGS_XML -f upload_pom.xml clean deploy
+
+cp -f ../SapMachine-Infrastructure/lib/ossrh/upload_pom.xml .
+sed -i "s/\${type}/jdk/g" upload_pom.xml
+sed -i "s/\${version}/$VERSION/g" upload_pom.xml
+sed -i "s/\${maven.name}/SapMachine JDK/g" upload_pom.xml
+if [[ $MAJOR_VERSION == 11 ]]; then
+    sed -i "s/\${macosx.platform.name}/osx/g" upload_pom.xml
+    sed -i "s/\${skip.macaarch}/true/g" upload_pom.xml
+else
+    sed -i "s/\${macosx.platform.name}/macos/g" upload_pom.xml
+    sed -i "s/\${skip.macaarch}/false/g" upload_pom.xml
+fi
+mvn -B --no-transfer-progress --settings $OSSRH_SETTINGS_XML -f upload_pom.xml clean deploy
 
 rm -rf ~/.gnupg/* $GPGSEC $OSSRH_SETTINGS_XML
