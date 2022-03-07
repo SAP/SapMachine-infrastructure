d73fc1225a03f8d18e14350614520bcba5e74a4e
Johannes Bechberger <johannes.bechberger@sap.com>
2022-03-07 17:08:04 +0100

    Add init script for JMC on M1

--- lib/install-deps-jmc-macos-aarch64.sh
+++ lib/install-deps-jmc-macos-aarch64.sh
@@ -17,10 +17,13 @@ fi
 
 cd "${WORKSPACE}"
 
-test -e sapmachine-jdk-11.0.14.1_windows-x64_bin.zip || wget https://github.com/SAP/SapMachine/releases/download/sapmachine-11.0.14.1/sapmachine-jdk-11.0.14.1_windows-x64_bin.zip
-unzip sapmachine-jdk-11.0.14.1_windows-x64_bin.zip
-export JAVA_HOME="`pwd`/sapmachine-jdk-11.0.14.1"
-export PATH="`pwd`/sapmachine-jdk-11.0.14.1/bin:$PATH"
+test -e zulu11.54.25-ca-jdk11.0.14.1-macosx_aarch64.tar.gz || wget https://cdn.azul.com/zulu/bin/zulu11.54.25-ca-jdk11.0.14.1-macosx_aarch64.tar.gz
+rm -fr zulu11.54.25-ca-jdk11.0.14.1-macosx_aarch64 boot_jdk
+tar xf zulu11.54.25-ca-jdk11.0.14.1-macosx_aarch64.tar.gz
+mv zulu11.54.25-ca-jdk11.0.14.1-macosx_aarch64 boot_jdk
+export JAVA_HOME="`pwd`/boot_jdk"
+export PATH="`pwd`/boot_jdk/bin:$PATH"
 test -e apache-maven-3.8.4-bin.zip || wget https://dlcdn.apache.org/maven/maven-3/3.8.4/binaries/apache-maven-3.8.4-bin.zip
+rm -fr apache-maven-3.8.4
 unzip apache-maven-3.8.4-bin.zip
 export PATH="`pwd`/apache-maven-3.8.4/bin:$PATH"
