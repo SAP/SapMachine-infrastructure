02151fbcb593fbe93d6061cd2c301a9e44cbc007
Christoph Langer <christoph.langer@sap.com>
2022-03-25 20:17:34 +0100

    JMC updates

--- lib/jmc/build.sh
+++ lib/jmc/build.sh
@@ -33,29 +33,10 @@ if [[ -z $NO_CHECKOUT ]]; then
   fi
 fi
 
-if [[ $UNAME == CYGWIN* ]]; then
-  mvn -f releng/third-party/pom.xml p2:site
-  mvn -f releng/third-party/pom.xml jetty:run &
-  sleep 5
+rm -rf target
 
-  mvn -f core/pom.xml clean package
+if [[ $UNAME == CYGWIN* ]]; then
+  cmd /c build.bat --packageJmc
 else
-  rm -fr target
   ./build.sh --packageJmc
 fi
-
-#mvn verify -P uitests
-#mvn verify
-
-#echo "check agent"
-#cd agent
-#mvn verify
-#cd ../core
-#mvn verify
-
-if [[ $JOB_NAME =~ .*linux_x86_64.* ]]; then FILE="linux.gtk.x86_64.tar.gz" ; fi
-if [[ $JOB_NAME =~ .*macos_aarch64.* ]]; then FILE="macosx.cocoa.aarch64.tar.gz" ; fi
-if [[ $JOB_NAME =~ .*macos_x86_64.* ]]; then FILE="macosx.cocoa.x86_64.tar.gz" ; fi
-if [[ $JOB_NAME =~ .*win.* ]]; then FILE="win32.win32.x86_64.zip" ; fi
-
-echo "jmc/target/products/org.openjdk.jmc-$FILE" > artifact.txt
