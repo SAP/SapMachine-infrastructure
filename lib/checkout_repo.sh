04a695de8b270e674dcd4972595fe9b152be86a1
Christoph Langer <christoph.langer@sap.com>
2022-05-23 14:17:21 +0200

    Fix typo

--- lib/checkout_repo.sh
+++ lib/checkout_repo.sh
@@ -5,5 +5,5 @@ mkdir SapMachine
 cd SapMachine
 git init
 git remote add origin $SAPMACHINE_GIT_REPOSITORY
-git fetch -depth 1 origin $GIT_REF
+git fetch --depth 1 origin $GIT_REF
 git checkout FETCH_HEAD
