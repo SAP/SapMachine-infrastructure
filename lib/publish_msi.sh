5ca8e59636be4070bd678e63536342fc2812172c
Christoph Langer <christoph.langer@sap.com>
2022-05-30 08:40:40 +0200

    Fix publishing of Windows msi

--- lib/publish_msi.sh
+++ lib/publish_msi.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 set -ex
 
+if [ "$SAPMACHINE_VERSION" != "" ]; then
+    VERSION_TAG=$SAPMACHINE_VERSION
+elif [ "$GIT_REF" != "" ]; then
+    VERSION_TAG=$GIT_REF
+else
+    echo "Neither SAPMACHINE_VERSION nor GIT_REF were set"
+    exit 1
+fi
+
 for MSIFILE in *.msi; do
-    python3 SapMachine-Infrastructure/lib/github_publish.py -t ${GIT_TAG_NAME} -a ${MSIFILE}
+    python3 SapMachine-Infrastructure/lib/github_publish.py -t ${VERSION_TAG} -a ${MSIFILE}
 done
