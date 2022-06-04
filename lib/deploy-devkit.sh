5dabd7a14e416be9e61d62e87a6b2db310cdd572
Christoph Langer <christoph.langer@sap.com>
2022-06-04 07:11:28 +0200

    Change location of devkit download for non Macs

--- lib/deploy-devkit.sh
+++ lib/deploy-devkit.sh
@@ -8,13 +8,21 @@ DEVKIT_ARTEFACT=$2
 DEVKIT_VERSION=$3
 DEVKIT_BASENAME=${DEVKIT_ARTEFACT}-${DEVKIT_VERSION}
 DEVKIT_ARCHIVE=${DEVKIT_BASENAME}.tar.gz
+# On Mac we try to avoid some downloads by keeping the devkit in a folder below the workdir.
+# This is not necessary/possible on other ephemeral Jenkins clients.
+if [[ $(uname) == Darwin ]]; then
+    DEVKIT_ARCHIVE_PATH="../${DEVKIT_ARCHIVE}"
+else
+    DEVKIT_ARCHIVE_PATH=${DEVKIT_ARCHIVE}
+fi
+
 
 if [ -d ${DEVKIT_BASENAME} ]; then
   echo Directory ${DEVKIT_BASENAME} already exists.
   exit 0
 fi
 
-if [ ! -f ../${DEVKIT_ARCHIVE} ]; then
+if [ ! -f ${DEVKIT_ARCHIVE_PATH} ]; then
   echo ${DEVKIT_ARCHIVE} does not exist, downloading...
   HTTPRC=`curl -L -s -I -u ${ART_USER}:${ART_PASSWORD} ${NEXUS_PATH}/${DEVKIT_GROUP_SLASH}/${DEVKIT_ARTEFACT}/${DEVKIT_VERSION}/${DEVKIT_ARCHIVE} | head -n 1 | cut -d$' ' -f2`
   if [[ $HTTPRC -eq 200 ]]; then
@@ -23,12 +31,12 @@ if [ ! -f ../${DEVKIT_ARCHIVE} ]; then
     echo Error: File not downloadable, request returned $HTTPRC.
     return -1
   fi
-  curl -L -s -o ../${DEVKIT_ARCHIVE} -u ${ART_USER}:${ART_PASSWORD} ${NEXUS_PATH}/${DEVKIT_GROUP_SLASH}/${DEVKIT_ARTEFACT}/${DEVKIT_VERSION}/${DEVKIT_ARCHIVE}
+  curl -L -o ${DEVKIT_ARCHIVE_PATH} -u ${ART_USER}:${ART_PASSWORD} ${NEXUS_PATH}/${DEVKIT_GROUP_SLASH}/${DEVKIT_ARTEFACT}/${DEVKIT_VERSION}/${DEVKIT_ARCHIVE}
 fi
 
 echo Extracting ${DEVKIT_ARCHIVE} to ${DEVKIT_BASENAME}...
 mkdir ${DEVKIT_BASENAME}
 pushd ${DEVKIT_BASENAME}
-tar xzf ../../${DEVKIT_ARCHIVE}
+tar xzf ../${DEVKIT_ARCHIVE_PATH}
 popd
 echo Extracted devkit.
