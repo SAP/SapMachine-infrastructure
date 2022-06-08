e5d4c0beb85138b61cb63d173c1d60fc7222be92
Christoph Langer <christoph.langer@sap.com>
2022-06-08 17:03:51 +0200

    Cleanups for new devkit logic

--- lib/deploy-devkit.sh
+++ lib/deploy-devkit.sh
@@ -8,30 +8,44 @@ DEVKIT_ARTEFACT=$2
 DEVKIT_VERSION=$3
 DEVKIT_BASENAME=${DEVKIT_ARTEFACT}-${DEVKIT_VERSION}
 DEVKIT_ARCHIVE=${DEVKIT_BASENAME}.tar.gz
-# On Mac we try to avoid some downloads by keeping the devkit in a folder below the workdir.
-# This is not necessary/possible on other ephemeral Jenkins clients.
+# We try to avoid unnecessary downloads by keeping the devkit around, e.g. as part of the docker image.
 if [[ $(uname) == Darwin ]]; then
     DEVKIT_ARCHIVE_PATH="../${DEVKIT_ARCHIVE}"
 else
     DEVKIT_ARCHIVE_PATH=${DEVKIT_ARCHIVE}
 fi
 
-
 if [ -d ${DEVKIT_BASENAME} ]; then
   echo Directory ${DEVKIT_BASENAME} already exists.
   exit 0
 fi
 
+if [[ $(uname) != Darwin ]]; then
+  # On Mac we had issues with a locally extracted version of the devkit. So we actually don't want to keep it around.
+  LOCAL_DEVKIT_PATH="/opt/devkits/${DEVKIT_BASENAME}"
+  if [ -d ${LOCAL_DEVKIT_PATH} ]; then
+    echo Directory ${LOCAL_DEVKIT_PATH} exists, linking it into workspace.
+    ln -s ${LOCAL_DEVKIT_PATH} ${DEVKIT_BASENAME}
+    exit 0
+  fi
+  # This fallback should not be necessary as the devkit should be extracted during docker build.
+  LOCAL_DEVKIT_ARCHIVE_PATH="/opt/devkits/${DEVKIT_ARCHIVE}"
+  if [ -f ${LOCAL_DEVKIT_ARCHIVE_PATH} ]; then
+    echo ${LOCAL_DEVKIT_ARCHIVE_PATH} exists.
+    ln -s ${LOCAL_DEVKIT_ARCHIVE_PATH} ${DEVKIT_ARCHIVE_PATH}
+  fi
+fi
+
 if [ ! -f ${DEVKIT_ARCHIVE_PATH} ]; then
   echo ${DEVKIT_ARCHIVE} does not exist, downloading...
-  HTTPRC=`curl -L -s -I -u ${ART_USER}:${ART_PASSWORD} ${NEXUS_PATH}/${DEVKIT_GROUP_SLASH}/${DEVKIT_ARTEFACT}/${DEVKIT_VERSION}/${DEVKIT_ARCHIVE} | head -n 1 | cut -d$' ' -f2`
+  HTTPRC=`curl -L -s -I -u ${ARTIFACTORY_CREDS} ${NEXUS_PATH}/${DEVKIT_GROUP_SLASH}/${DEVKIT_ARTEFACT}/${DEVKIT_VERSION}/${DEVKIT_ARCHIVE} | head -n 1 | cut -d$' ' -f2`
   if [[ $HTTPRC -eq 200 ]]; then
     echo File seems to be downloadable, request returned $HTTPRC.
   else
     echo Error: File not downloadable, request returned $HTTPRC.
     return -1
   fi
-  curl -L -s -o ${DEVKIT_ARCHIVE_PATH} -u ${ART_USER}:${ART_PASSWORD} ${NEXUS_PATH}/${DEVKIT_GROUP_SLASH}/${DEVKIT_ARTEFACT}/${DEVKIT_VERSION}/${DEVKIT_ARCHIVE}
+  curl -L -s -o ${DEVKIT_ARCHIVE_PATH} -u ${ARTIFACTORY_CREDS} ${NEXUS_PATH}/${DEVKIT_GROUP_SLASH}/${DEVKIT_ARTEFACT}/${DEVKIT_VERSION}/${DEVKIT_ARCHIVE}
 fi
 
 echo Extracting ${DEVKIT_ARCHIVE} to ${DEVKIT_BASENAME}...
