42a2a3ef22387304a4139089d27e209737d33514
Christoph Langer <christoph.langer@sap.com>
2022-06-10 14:36:31 +0200

    Overhaul devkit logic and add a devkit for Windows as well

--- lib/deploy-devkit.sh
+++ lib/deploy-devkit.sh
@@ -8,49 +8,58 @@ DEVKIT_ARTEFACT=$2
 DEVKIT_VERSION=$3
 DEVKIT_BASENAME=${DEVKIT_ARTEFACT}-${DEVKIT_VERSION}
 DEVKIT_ARCHIVE=${DEVKIT_BASENAME}.tar.gz
-# We try to avoid unnecessary downloads by keeping the devkit around, e.g. as part of the docker image.
-if [[ $(uname) == Darwin ]]; then
-    DEVKIT_ARCHIVE_PATH="../${DEVKIT_ARCHIVE}"
-else
-    DEVKIT_ARCHIVE_PATH=${DEVKIT_ARCHIVE}
-fi
 
-if [ -d ${DEVKIT_BASENAME} ]; then
-  echo Directory ${DEVKIT_BASENAME} already exists.
-  exit 0
-fi
+# We try to avoid unnecessary work by preparing the build agents with the devkit.
+# However, should the local devkit of the requested version be missing, we'll download and extract it here.
 
+# Here we check if the extracted devkit is present.
+# On our Mac runners we had issues that an already extracted devkit folder went stale, so we don't do this for mac atm.
 if [[ $(uname) != Darwin ]]; then
-  # On Mac we had issues with a locally extracted version of the devkit. So we actually don't want to keep it around.
-  LOCAL_DEVKIT_PATH="/opt/devkits/${DEVKIT_BASENAME}"
-  if [ -d ${LOCAL_DEVKIT_PATH} ]; then
-    echo Directory ${LOCAL_DEVKIT_PATH} exists, linking it into workspace.
-    ln -s ${LOCAL_DEVKIT_PATH} ${DEVKIT_BASENAME}
+  if [[ $UNAME == CYGWIN* ]]; then
+    DEVKIT_PATH="/cygdrive/c/devkits/${DEVKIT_BASENAME}"
+  else
+    DEVKIT_PATH="opt/devkits/${DEVKIT_BASENAME}"
+  fi
+
+  if [ -d ${DEVKIT_PATH} ]; then
+    echo Devkit directory ${DEVKIT_PATH} exists, using it.
+    echo "${DEVKIT_PATH}" > devkitlocation.txt
     exit 0
   fi
-  # This fallback should not be necessary as the devkit should be extracted during docker build.
-  LOCAL_DEVKIT_ARCHIVE_PATH="/opt/devkits/${DEVKIT_ARCHIVE}"
-  if [ -f ${LOCAL_DEVKIT_ARCHIVE_PATH} ]; then
-    echo ${LOCAL_DEVKIT_ARCHIVE_PATH} exists.
-    ln -s ${LOCAL_DEVKIT_ARCHIVE_PATH} ${DEVKIT_ARCHIVE_PATH}
-  fi
+fi
+
+# OK, the devkit directory is not there. Do we already have the archive?
+DEVKIT_PATH="${DEVKIT_BASENAME}"
+
+if [[ $(uname) == Darwin ]]; then
+  DEVKIT_ARCHIVE_PATH="../${DEVKIT_ARCHIVE}"
+elif [[ $UNAME == CYGWIN* ]]; then
+  DEVKIT_ARCHIVE_PATH="/cygdrive/c/devkits/${DEVKIT_ARCHIVE}"
+else
+  DEVKIT_ARCHIVE_PATH="/opt/devkits/${DEVKIT_ARCHIVE}"
 fi
 
 if [ ! -f ${DEVKIT_ARCHIVE_PATH} ]; then
-  echo ${DEVKIT_ARCHIVE} does not exist, downloading...
-  HTTPRC=`curl -L -s -I -u ${ARTIFACTORY_CREDS} ${NEXUS_PATH}/${DEVKIT_GROUP_SLASH}/${DEVKIT_ARTEFACT}/${DEVKIT_VERSION}/${DEVKIT_ARCHIVE} | head -n 1 | cut -d$' ' -f2`
-  if [[ $HTTPRC -eq 200 ]]; then
-    echo File seems to be downloadable, request returned $HTTPRC.
-  else
-    echo Error: File not downloadable, request returned $HTTPRC.
-    return -1
+  echo Devkit archive ${DEVKIT_ARCHIVE_PATH} does not exist, need to download it.
+  if [[ $(uname) != Darwin ]]; then
+    DEVKIT_ARCHIVE_PATH="${DEVKIT_ARCHIVE}"
   fi
-  curl -L -s -o ${DEVKIT_ARCHIVE_PATH} -u ${ARTIFACTORY_CREDS} ${NEXUS_PATH}/${DEVKIT_GROUP_SLASH}/${DEVKIT_ARTEFACT}/${DEVKIT_VERSION}/${DEVKIT_ARCHIVE}
 fi
 
-echo Extracting ${DEVKIT_ARCHIVE} to ${DEVKIT_BASENAME}...
-mkdir ${DEVKIT_BASENAME}
-pushd ${DEVKIT_BASENAME}
+DOWNLOAD_URL=${NEXUS_PATH}/${DEVKIT_GROUP_SLASH}/${DEVKIT_ARTEFACT}/${DEVKIT_VERSION}/${DEVKIT_ARCHIVE}
+HTTPRC=`curl -L -s -I -u ${ARTIFACTORY_CREDS} ${DOWNLOAD_URL} | head -n 1 | cut -d$' ' -f2`
+if [[ $HTTPRC -ne 200 ]]; then
+  echo Error: ${DOWNLOAD_URL} is not downloadable, request returned $HTTPRC.
+  return -1
+fi
+echo Downloading ${DOWNLOAD_URL} to ...
+curl -L -s -o ${DEVKIT_ARCHIVE_PATH} -u ${ARTIFACTORY_CREDS} ${DOWNLOAD_URL}
+
+echo Extracting ${DEVKIT_ARCHIVE} to ${DEVKIT_PATH}...
+mkdir ${DEVKIT_PATH}
+pushd ${DEVKIT_PATH}
 tar xzf ../${DEVKIT_ARCHIVE_PATH}
 popd
 echo Extracted devkit.
+
+echo "${DEVKIT_PATH}" > devkitlocation.txt
