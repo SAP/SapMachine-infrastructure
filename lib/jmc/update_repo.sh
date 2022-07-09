217ca489f21a87042610719351696b7bdee01e26
Christoph Langer <christoph.langer@sap.com>
2022-07-09 21:45:43 +0200

    Updates for JMC repo job

--- lib/jmc/update_repo.sh
+++ lib/jmc/update_repo.sh
@@ -7,7 +7,8 @@ if  [[ -z "$GIT_USER" ]] | [[ -z "$GIT_PASSWORD" ]]; then
 fi
 
 OPENJDK_JMC_REPOSITORY="https://github.com/openjdk/jmc.git"
-SAPMACHINE_JMC_REPOSITORY="https://${GIT_USER}:${GIT_PASSWORD}@github.com/SAP/jmc.git"
+SAPMACHINE_JMC_REPOSITORY="https://github.com/SAP/jmc.git"
+SAPMACHINE_JMC_REPOSITORY_PUSH="https://${GIT_USER}:${GIT_PASSWORD}@github.com/SAP/jmc.git"
 
 REPO_PATH=jmc
 
@@ -15,18 +16,22 @@ cd $WORKSPACE
 
 # first checkout jmc repository, branch master and pull
 if [ ! -d $REPO_PATH ]; then
-  git clone "$OPENJDK_JMC_REPOSITORY" $REPO_PATH
+  git clone -b sap "$SAPMACHINE_JMC_REPOSITORY" $REPO_PATH
   cd $REPO_PATH
+  git remote add upstream $OPENJDK_JMC_REPOSITORY
+  git fetch upstream
+  git checkout -b master upstream/master
+  git checkout -b jmc8 upstream/jmc8
 else
   cd $REPO_PATH
-  git checkout master
   git fetch origin
-  git pull
+  git fetch upstream
 fi
 
-git push --follow-tags $SAPMACHINE_JMC_REPOSITORY master:master
-
-git checkout jmc8
+git checkout master
 git pull
+git push --follow-tags $SAPMACHINE_JMC_REPOSITORY_PUSH master:master
 
-git push --follow-tags $SAPMACHINE_JMC_REPOSITORY jmc8:jmc8
+git checkout upstream/jmc8
+git pull
+git push --follow-tags $SAPMACHINE_JMC_REPOSITORY_PUSH jmc8:jmc8
