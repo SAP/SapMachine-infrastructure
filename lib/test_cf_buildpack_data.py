a53e87adb001cbe6a31189f306089f4b03e04641
Christoph Langer <christoph.langer@sap.com>
2022-08-02 01:40:11 +0200

    Improve cf test script to handle OpenJDK 1.8. entries and print some more error output

--- lib/test_cf_buildpack_data.py
+++ lib/test_cf_buildpack_data.py
@@ -1,5 +1,5 @@
 '''
-Copyright (c) 2001-2018 by SAP SE, Walldorf, Germany.
+Copyright (c) 2018-2022 by SAP SE, Walldorf, Germany.
 All rights reserved. Confidential and proprietary.
 '''
 
@@ -10,7 +10,7 @@ from urllib.request import urlopen, Request
 from urllib.error import HTTPError
 
 cf_yml_url = 'https://sap.github.io/SapMachine/assets/cf/jre/linux/x86_64/index.yml'
-cf_version_pattern = re.compile('(((\d+)\.(\d+)\.(\d+))_(\d+)\.(\d+))\.b(\d+)')
+cf_version_pattern = re.compile('(\d+)\.(\d+)\.(\d+)_(\d+)(\.(\d+))?(\.b(\d+))?')
 
 def main(argv=None):
     request = Request(cf_yml_url)
@@ -19,6 +19,7 @@ def main(argv=None):
     try:
         response = urlopen(request)
     except HTTPError as e:
+        print(str.format('Error loading {0}', cf_yml_url))
         return 1
 
     try:
@@ -29,25 +30,29 @@ def main(argv=None):
             match = cf_version_pattern.match(version)
 
             if match is None:
+                print(str.format('Error: No match for {0}', version))
                 return 1
 
             url = runtimes[version]
-            print(str.format('found version "{0}" with url "{1}"', version, url))
+            print(str.format('Found version "{0}" with url "{1}"', version, url))
             request = Request(url)
 
             try:
                 response = urlopen(request)
                 if response.code != 200:
+                    print(str.format('Error ({0}) opening {1}', response.code, url))
                     return 1
 
             except HTTPError as e:
+                print(str.format('HTTPError opening {0}: {1}', url, e))
                 return 1
 
     except yaml.YAMLError as e:
+        print(str.format('YAMLError: {0}', e))
         return 1
 
     if versions_found == 0:
-        print('no versions found in buildpack data ...')
+        print('Error: No versions found in buildpack data')
         return 1
 
     return 0
