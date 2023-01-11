a8c07f053023147ffd507721b558dafe1f06b79c
Christoph Langer <christoph.langer@sap.com>
2023-01-11 19:34:24 +0100

    Fix template syntax

--- lib/generate_dockerfiles.py
+++ lib/generate_dockerfiles.py
@@ -24,7 +24,7 @@ RUN export GNUPGHOME="$$(mktemp -d)" \\
     && gpg --batch --keyserver hkps://keys.openpgp.org --recv-keys CACB9FE09150307D1D22D82962754C3B3ABCFE23 \\
     && gpg --batch --export --armor 'CACB 9FE0 9150 307D 1D22 D829 6275 4C3B 3ABC FE23' > /etc/apt/trusted.gpg.d/sapmachine.gpg.asc \\
     && gpgconf --kill all && rm -rf "$$GNUPGHOME" \\
-    && echo "deb http://dist.sapmachine.io/debian/$(dpkg --print-architecture)/ ./" > /etc/apt/sources.list.d/sapmachine.list \\
+    && echo "deb http://dist.sapmachine.io/debian/$$(dpkg --print-architecture)/ ./" > /etc/apt/sources.list.d/sapmachine.list \\
     && apt-get update \\
     && apt-get -y --no-install-recommends install ${version} \\
     && rm -rf /var/lib/apt/lists/*
