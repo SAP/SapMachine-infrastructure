4313da984edae8b09cf009b7310f1b4e5b159b99
Christoph Langer <christoph.langer@sap.com>
2023-02-06 11:28:05 +0100

    Improve publish steps

--- lib/publish.sh
+++ lib/publish.sh
@@ -17,8 +17,8 @@ if [[ -z $SAPMACHINE_GIT_REPOSITORY ]]; then
 fi
 
 if [[ -z "$SAPMACHINE_VERSION" ]]; then
-    echo "SAPMACHINE_VERSION not set"
-    exit 1
+  echo "SAPMACHINE_VERSION not set"
+  exit 1
 fi
 python3 SapMachine-Infrastructure/lib/github_publish.py -t $SAPMACHINE_VERSION $PRE_RELEASE_OPT || true
 
@@ -31,11 +31,11 @@ ARCHIVE_NAME_SYMBOLS="$(cat symbols_bundle_name.txt)"
 HAS_ZIP=$(ls sapmachine-jdk-*_bin.zip | wc -l)
 
 if [ "$HAS_ZIP" -lt "1" ]; then
-    ARCHIVE_SUM_JDK="$(echo $ARCHIVE_NAME_JDK | sed 's/tar\.gz/sha256\.txt/')"
-    ARCHIVE_SUM_JRE="$(echo $ARCHIVE_NAME_JRE | sed 's/tar\.gz/sha256\.txt/')"
+  ARCHIVE_SUM_JDK="$(echo $ARCHIVE_NAME_JDK | sed 's/tar\.gz/sha256\.txt/')"
+  ARCHIVE_SUM_JRE="$(echo $ARCHIVE_NAME_JRE | sed 's/tar\.gz/sha256\.txt/')"
 else
-    ARCHIVE_SUM_JDK="$(echo $ARCHIVE_NAME_JDK | sed 's/zip/sha256\.txt/')"
-    ARCHIVE_SUM_JRE="$(echo $ARCHIVE_NAME_JRE | sed 's/zip/sha256\.txt/')"
+  ARCHIVE_SUM_JDK="$(echo $ARCHIVE_NAME_JDK | sed 's/zip/sha256\.txt/')"
+  ARCHIVE_SUM_JRE="$(echo $ARCHIVE_NAME_JRE | sed 's/zip/sha256\.txt/')"
 fi
 ARCHIVE_SUM_SYMBOLS="$(echo $ARCHIVE_NAME_SYMBOLS | sed 's/tar\.gz/sha256\.txt/')"
 
@@ -51,17 +51,25 @@ python3 SapMachine-Infrastructure/lib/github_publish.py -t $SAPMACHINE_VERSION -
 python3 SapMachine-Infrastructure/lib/github_publish.py -t $SAPMACHINE_VERSION -a "${ARCHIVE_SUM_SYMBOLS}"
 
 if [ $UNAME == Darwin ]; then
-    DMG_NAME_JDK="$(cat jdk_dmg_name.txt)"
-    DMG_NAME_JRE="$(cat jre_dmg_name.txt)"
+  DMG_NAME_JDK="$(cat jdk_dmg_name.txt)"
+  DMG_NAME_JRE="$(cat jre_dmg_name.txt)"
 
-    DMG_SUM_JDK="$(echo $DMG_NAME_JDK | sed 's/dmg/dmg\.sha256\.txt/')"
-    DMG_SUM_JRE="$(echo $DMG_NAME_JRE | sed 's/dmg/dmg\.sha256\.txt/')"
+  DMG_SUM_JDK="$(echo $DMG_NAME_JDK | sed 's/dmg/dmg\.sha256\.txt/')"
+  DMG_SUM_JRE="$(echo $DMG_NAME_JRE | sed 's/dmg/dmg\.sha256\.txt/')"
 
-    shasum -a 256 $DMG_NAME_JDK > $DMG_SUM_JDK
-    shasum -a 256 $DMG_NAME_JRE > $DMG_SUM_JRE
+  shasum -a 256 $DMG_NAME_JDK > $DMG_SUM_JDK
+  shasum -a 256 $DMG_NAME_JRE > $DMG_SUM_JRE
 
-    python3 SapMachine-Infrastructure/lib/github_publish.py -t $SAPMACHINE_VERSION -a "${DMG_NAME_JDK}"
-    python3 SapMachine-Infrastructure/lib/github_publish.py -t $SAPMACHINE_VERSION -a "${DMG_NAME_JRE}"
-    python3 SapMachine-Infrastructure/lib/github_publish.py -t $SAPMACHINE_VERSION -a "${DMG_SUM_JDK}"
-    python3 SapMachine-Infrastructure/lib/github_publish.py -t $SAPMACHINE_VERSION -a "${DMG_SUM_JRE}"
+  python3 SapMachine-Infrastructure/lib/github_publish.py -t $SAPMACHINE_VERSION -a "${DMG_NAME_JDK}"
+  python3 SapMachine-Infrastructure/lib/github_publish.py -t $SAPMACHINE_VERSION -a "${DMG_NAME_JRE}"
+  python3 SapMachine-Infrastructure/lib/github_publish.py -t $SAPMACHINE_VERSION -a "${DMG_SUM_JDK}"
+  python3 SapMachine-Infrastructure/lib/github_publish.py -t $SAPMACHINE_VERSION -a "${DMG_SUM_JRE}"
+fi
+
+if [[ $UNAME == CYGWIN* ]]; then
+  for MSIFILE in *.msi; do
+    shasum -a 256 $MSIFILE > ${MSIFILE}.sha256.txt
+    python3 SapMachine-Infrastructure/lib/github_publish.py -t ${SAPMACHINE_VERSION} -a ${MSIFILE}
+    python3 SapMachine-Infrastructure/lib/github_publish.py -t ${SAPMACHINE_VERSION} -a ${MSIFILE}.sha256.txt
+  done
 fi
