f1639117b9381dc6e0e8108ad4fc07104eceafbf
Christoph Langer <christoph.langer@sap.com>
2023-02-10 16:52:44 +0100

    Try to improve handling of errors in get_configure_opts.py

--- lib/build.sh
+++ lib/build.sh
@@ -56,7 +56,12 @@ if [[ ! -z $JDK_BUILD ]]; then
   _BUILD_NUMBER=" -b $BUILD_NUMBER"
 fi
 
-eval _CONFIGURE_OPTS=($(python3 ../SapMachine-Infrastructure/lib/get_configure_opts.py $_GIT_TAG $_BUILD_NUMBER))
+_CONFIGURE_OPTS=$(python3 ../SapMachine-Infrastructure/lib/get_configure_opts.py $_GIT_TAG $_BUILD_NUMBER)
+if ($? ne 0)
+  echo "Error in get_configure_opts.py"
+  exit 1
+fi
+eval _CONFIGURE_OPTS=(${_CONFIGURE_OPTS})
 
 (set -x &&
 bash ./configure \
