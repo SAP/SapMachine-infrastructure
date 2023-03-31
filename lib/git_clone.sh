c2fa1ddf8202bc7de04a938c6ce8be0913ebeb85
Christoph Langer <christoph.langer@sap.com>
2023-03-31 09:22:37 +0200

    small fix

--- lib/git_clone.sh
+++ lib/git_clone.sh
@@ -14,5 +14,5 @@ fi
 "$GIT_TOOL" --version
 set -ex
 "$GIT_TOOL" init $2 && cd $2
-GIT_TERMINAL_PROMPT=0 "$GIT_TOOL" $GIT_CREDENTIALS fetch --depth 1 $1 $3
+GIT_TERMINAL_PROMPT=0 eval "$GIT_TOOL" $GIT_CREDENTIALS fetch --depth 1 $1 $3
 "$GIT_TOOL" checkout --detach FETCH_HEAD
