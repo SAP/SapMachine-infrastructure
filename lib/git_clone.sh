7b7f71ffed763dea6d011603614f3383c933c23b
Christoph Langer <christoph.langer@sap.com>
2023-03-31 09:15:05 +0200

    Honor credentials in git_clone

--- lib/git_clone.sh
+++ lib/git_clone.sh
@@ -7,8 +7,12 @@ else
   GIT_TOOL=git
 fi
 
+if [ ! -z $GIT_USER ]; then
+  GIT_CREDENTIALS="-c credential.helper='!f() { sleep 1; echo \"username=${GIT_USER}\"; echo \"password=${GIT_PASSWORD}\"; }; f'"
+fi
+
 "$GIT_TOOL" --version
 set -ex
 "$GIT_TOOL" init $2 && cd $2
-GIT_TERMINAL_PROMPT=0 "$GIT_TOOL" fetch --depth 1 $1 $3
+GIT_TERMINAL_PROMPT=0 "$GIT_TOOL" $GIT_CREDENTIALS fetch --depth 1 $1 $3
 "$GIT_TOOL" checkout --detach FETCH_HEAD
