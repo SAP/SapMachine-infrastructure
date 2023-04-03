347938e1c96ec052ebfd1f5d005bc0ad1c0d7e7a
Christoph Langer <christoph.langer@sap.com>
2023-04-03 12:11:31 +0200

    Use deep fetch for PRs since they need to be merged

--- lib/git_clone.sh
+++ lib/git_clone.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 set -e
 
+DEPTH_OPTION="--depth 1"
+if [ ! -z $4 ]; then
+  if [[ $4 == deep ]]; then
+    DEPTH_OPTION=
+  fi
+fi
+
 if [[ `uname` == CYGWIN* ]]; then
   GIT_TOOL="/cygdrive/c/Program Files/Git/cmd/git.exe"
   GIT_TOOL_FOR_EVAL="/cygdrive/c/Program\ Files/Git/cmd/git.exe"
@@ -16,5 +23,5 @@ fi
 "$GIT_TOOL" --version
 set -ex
 "$GIT_TOOL" init $2 && cd $2
-GIT_TERMINAL_PROMPT=0 eval "$GIT_TOOL_FOR_EVAL" $GIT_CREDENTIALS fetch --depth 1 $1 $3
+GIT_TERMINAL_PROMPT=0 eval "$GIT_TOOL_FOR_EVAL" $GIT_CREDENTIALS fetch $DEPTH_OPTION $1 $3
 "$GIT_TOOL" checkout --detach FETCH_HEAD
