b2e3f128d8e2711fb9b1f4b8858e6d45d7b1bf8b
Christoph Langer <christoph.langer@sap.com>
2023-04-13 23:24:29 +0200

    Fixes/cleanups for download of MacOS artifacts from internal artifactory

--- lib/download_from_artifactory.py
+++ lib/download_from_artifactory.py
@@ -3,30 +3,39 @@ Copyright (c) 2023 by SAP SE, Walldorf, Germany.
 All rights reserved. Confidential and proprietary.
 '''
 
+import argparse
 import os
 import requests
+import sys
 
 prefix = os.getenv("DOWNLOAD_URL_PREFIX")
 symbols_prefix = os.getenv("DOWNLOAD_SYMBOLS_URL_PREFIX")
 version = (os.getenv("SAPMACHINE_VERSION")).removeprefix("sapmachine-")
 chunk_size = 4096
 
-def download_from_artifactory(u, dname):
-    print(f"Downloading {u} to file {dname}")
-    with requests.get(u, auth=(os.getenv("ARTIFACTORY_USER"), os.getenv("ARTIFACTORY_PWD")), stream=True) as r:
-        with open(dname, 'wb') as f:
+def download_from_artifactory(url, filename):
+    print(f"Downloading {url} to file {filename}")
+    with requests.get(url, auth=(os.getenv("ARTIFACTORY_USER"), os.getenv("ARTIFACTORY_PWD")), stream=True) as r:
+        with open(filename, 'wb') as f:
             for chunk in r.iter_content(chunk_size):
                 if chunk:
                     f.write(chunk)
 
+def main(argv=None):
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-p', '--platform', help='The platform of the artifacts', metavar='PLATFORM')
+    args = parser.parse_args()
 
-for platform in ["aarch", "intel"]:
     for type in ["jre", "jdk"]:
         for suffix in ["tar.gz", "dmg"]:
-            u = f"{prefix}/sapmachine-{type}_darwin{platform}64/{version}/sapmachine-{type}_darwin{platform}64-{version}-notarized.{suffix}"
+            url = f"{prefix}/sapmachine-{type}_darwin{args.platform}64/{version}/sapmachine-{type}_darwin{args.platform}64-{version}-notarized.{suffix}"
             dsuffix = "TGZ" if (suffix == "tar.gz") else "DMG"
-            f = f"{platform.upper()}_{type.upper()}_{dsuffix}"
-            download_from_artifactory(u, f)
-    u = f"{symbols_prefix}/sapmachine-symbols_darwin{platform}64/{version}/sapmachine-symbols_darwin{platform}64-{version}.tar.gz"
-    f = f"{platform.upper()}_SYMBOLS"
-    download_from_artifactory(u, f)
+            filename = f"{args.platform.upper()}_{type.upper()}_{dsuffix}"
+            download_from_artifactory(url, filename)
+    url = f"{symbols_prefix}/sapmachine-symbols_darwin{args.platform}64/{version}/sapmachine-symbols_darwin{args.platform}64-{version}.tar.gz"
+    filename = f"{args.platform.upper()}_SYMBOLS"
+    download_from_artifactory(url, filename)
+    return 0
+
+if __name__ == "__main__":
+    sys.exit(main())
