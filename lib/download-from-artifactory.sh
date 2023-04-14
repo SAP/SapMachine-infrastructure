f5805071910ed1559f5eb1d3692f7db18ea39ddd
Christoph Langer <christoph.langer@sap.com>
2023-04-14 12:27:10 +0200

    Use a shell script and curl for downloading macos artifacts from Artifactory

---
+++ lib/download-from-artifactory.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+set -ex
+
+download_artifact() {
+  DOWNLOAD_URL=$1
+  TARGET_PATH=$2
+
+  (set -x && curl --version)
+  HTTPRC=`curl -L -s -I -u ${ARTIFACTORY_CREDS} ${DOWNLOAD_URL} | head -n 1 | cut -d$' ' -f2`
+  if [[ $HTTPRC -ne 200 ]]; then
+    echo Error: ${DOWNLOAD_URL} is not downloadable, request returned $HTTPRC.
+    exit -1
+  fi
+  echo Downloading ${DOWNLOAD_URL} to ${TARGET_PATH}...
+  curl -L -s -o ${TARGET_PATH} -u ${ARTIFACTORY_CREDS} ${DOWNLOAD_URL}
+}
+
+PLATFORM_ARG=$1
+SMVERS=${SAPMACHINE_VERSION#sapmachine-}
+
+JDK_TGZ_NAME=sapmachine-jdk_darwin${PLATFORM_ARG}64-${SMVERS}${NOTARIZED_SUFFIX}.tar.gz
+JDK_TGZ_URL=${BINARY_SOURCE}/sapmachine-jdk_darwin${PLATFORM_ARG}64/${SMVERS}/${JDK_TGZ_NAME}
+
+JDK_DMG_NAME=sapmachine-jdk_darwin${PLATFORM_ARG}64-${SMVERS}${NOTARIZED_SUFFIX}.dmg
+JDK_DMG_URL=${BINARY_SOURCE}/sapmachine-jdk_darwin${PLATFORM_ARG}64/${SMVERS}/${JDK_DMG_NAME}
+
+JRE_TGZ_NAME=sapmachine-jre_darwin${PLATFORM_ARG}64-${SMVERS}${NOTARIZED_SUFFIX}.tar.gz
+JRE_TGZ_URL=${BINARY_SOURCE}/sapmachine-jre_darwin${PLATFORM_ARG}64/${SMVERS}/${JRE_TGZ_NAME}
+
+JRE_DMG_NAME=sapmachine-jre_darwin${PLATFORM_ARG}64-${SMVERS}${NOTARIZED_SUFFIX}.dmg
+JRE_DMG_URL=${BINARY_SOURCE}/sapmachine-jre_darwin${PLATFORM_ARG}64/${SMVERS}/${JRE_DMG_NAME}
+
+SYMBOLS_NAME=sapmachine-symbols_darwin${PLATFORM_ARG}64-${SMVERS}.tar.gz
+SYMBOLS_URL=${SYMBOL_SOURCE}/sapmachine-symbols_darwin${PLATFORM_ARG}64/${SMVERS}/${}
+
+download_artifact ${JDK_TGZ_URL} ${JDK_TGZ_NAME}
+download_artifact ${JDK_DMG_URL} ${JDK_DMG_NAME}
+download_artifact ${JRE_TGZ_URL} ${JRE_TGZ_NAME}
+download_artifact ${JRE_DMG_URL} ${JRE_DMG_NAME}
+download_artifact ${SYMBOLS_URL} ${SYMBOLS_NAME}
