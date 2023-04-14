3c9d04d41992f087bd133991f6f2805fa15b0fba
Christoph Langer <christoph.langer@sap.com>
2023-04-14 15:20:41 +0200

    Add publishing to new macos publish job

--- lib/download-from-artifactory.sh
+++ lib/download-from-artifactory.sh
@@ -15,26 +15,33 @@ download_artifact() {
   curl -L -s -o ${TARGET_PATH} -u ${ARTIFACTORY_CREDS} ${DOWNLOAD_URL}
 }
 
-PLATFORM_ARG=$1
-SMVERS=${SAPMACHINE_VERSION#sapmachine-}
+# aarch64 or x64
+PUBLISH_PLATFORM=$1
 
-JDK_TGZ_NAME=sapmachine-jdk_darwin${PLATFORM_ARG}64-${SMVERS}${NOTARIZED_SUFFIX}.tar.gz
-JDK_TGZ_URL=${BINARY_SOURCE}/sapmachine-jdk_darwin${PLATFORM_ARG}64/${SMVERS}/${JDK_TGZ_NAME}
+SOURCE_PLATFORM=$PUBLISH_PLATFORM
+if [ "$SOURCE_PLATFORM" = "x64" ]; then
+    SOURCE_PLATFORM=intel64
+fi
 
-JDK_DMG_NAME=sapmachine-jdk_darwin${PLATFORM_ARG}64-${SMVERS}${NOTARIZED_SUFFIX}.dmg
-JDK_DMG_URL=${BINARY_SOURCE}/sapmachine-jdk_darwin${PLATFORM_ARG}64/${SMVERS}/${JDK_DMG_NAME}
+read VERSION OS_NAME<<< $(python3 ${WORKSPACE}/SapMachine-infrastructure/lib/publish_osx_get_version_osname.py -t ${SAPMACHINE_VERSION})
 
-JRE_TGZ_NAME=sapmachine-jre_darwin${PLATFORM_ARG}64-${SMVERS}${NOTARIZED_SUFFIX}.tar.gz
-JRE_TGZ_URL=${BINARY_SOURCE}/sapmachine-jre_darwin${PLATFORM_ARG}64/${SMVERS}/${JRE_TGZ_NAME}
+JDK_TGZ_NAME=sapmachine-jdk-${VERSION}_${OS_NAME}-${PUBLISH_PLATFORM}_bin.tar.gz
+JDK_TGZ_URL=${BINARY_SOURCE}/sapmachine-jdk_darwin${SOURCE_PLATFORM}/${VERSION}/sapmachine-jdk_darwin${SOURCE_PLATFORM}-${VERSION}${NOTARIZED_SUFFIX}.tar.gz
 
-JRE_DMG_NAME=sapmachine-jre_darwin${PLATFORM_ARG}64-${SMVERS}${NOTARIZED_SUFFIX}.dmg
-JRE_DMG_URL=${BINARY_SOURCE}/sapmachine-jre_darwin${PLATFORM_ARG}64/${SMVERS}/${JRE_DMG_NAME}
+JDK_DMG_NAME=sapmachine-jdk-${VERSION}_${OS_NAME}-${PUBLISH_PLATFORM}_bin.dmg
+JDK_DMG_URL=${BINARY_SOURCE}/sapmachine-jdk_darwin${SOURCE_PLATFORM}/${VERSION}/sapmachine-jdk_darwin${SOURCE_PLATFORM}-${VERSION}${NOTARIZED_SUFFIX}.dmg
 
-SYMBOLS_NAME=sapmachine-symbols_darwin${PLATFORM_ARG}64-${SMVERS}.tar.gz
-SYMBOLS_URL=${SYMBOL_SOURCE}/sapmachine-symbols_darwin${PLATFORM_ARG}64/${SMVERS}/${SYMBOLS_NAME}
+JRE_TGZ_NAME=sapmachine-jre-${VERSION}_${OS_NAME}-${PUBLISH_PLATFORM}_bin.tar.gz
+JRE_TGZ_URL=${BINARY_SOURCE}/sapmachine-jre_darwin${SOURCE_PLATFORM}/${VERSION}/sapmachine-jre_darwin${SOURCE_PLATFORM}-${VERSION}${NOTARIZED_SUFFIX}.tar.gz
+
+JRE_DMG_NAME=sapmachine-jre-${VERSION}_${OS_NAME}-${PUBLISH_PLATFORM}_bin.dmg
+JRE_DMG_URL=${BINARY_SOURCE}/sapmachine-jre_darwin${SOURCE_PLATFORM}/${VERSION}/sapmachine-jre_darwin${SOURCE_PLATFORM}-${VERSION}${NOTARIZED_SUFFIX}.dmg
+
+SYMBOLS_TGZ_NAME=sapmachine-jdk-${VERSION}_${OS_NAME}-${PUBLISH_PLATFORM}_bin-symbols.tar.gz
+SYMBOLS_URL=${SYMBOL_SOURCE}/sapmachine-symbols_darwin${SOURCE_PLATFORM}/${VERSION}/sapmachine-symbols_darwin${SOURCE_PLATFORM}-${VERSION}.tar.gz
 
 download_artifact ${JDK_TGZ_URL} ${JDK_TGZ_NAME}
 download_artifact ${JDK_DMG_URL} ${JDK_DMG_NAME}
 download_artifact ${JRE_TGZ_URL} ${JRE_TGZ_NAME}
 download_artifact ${JRE_DMG_URL} ${JRE_DMG_NAME}
-download_artifact ${SYMBOLS_URL} ${SYMBOLS_NAME}
+download_artifact ${SYMBOLS_URL} ${SYMBOLS_TGZ_NAME}
--- lib/publish_osx.sh
+++ lib/publish_osx.sh
@@ -1,72 +1,45 @@
 #!/bin/bash
 set -ex
 
+# aarch64 or x64
+PUBLISH_PLATFORM=$1
+
 read VERSION OS_NAME<<< $(python3 ${WORKSPACE}/SapMachine-infrastructure/lib/publish_osx_get_version_osname.py -t ${SAPMACHINE_VERSION})
 
-INTEL_ARCHIVE_NAME_JDK="sapmachine-jdk-${VERSION}_${OS_NAME}-x64_bin.tar.gz"
-INTEL_ARCHIVE_NAME_JRE="sapmachine-jre-${VERSION}_${OS_NAME}-x64_bin.tar.gz"
-INTEL_ARCHIVE_NAME_SYMBOLS="sapmachine-jdk-${VERSION}_${OS_NAME}-x64_bin-symbols.tar.gz"
-INTEL_DMG_NAME_JDK="sapmachine-jdk-${VERSION}_${OS_NAME}-x64_bin.dmg"
-INTEL_DMG_NAME_JRE="sapmachine-jre-${VERSION}_${OS_NAME}-x64_bin.dmg"
-AARCH_ARCHIVE_NAME_JDK="sapmachine-jdk-${VERSION}_${OS_NAME}-aarch64_bin.tar.gz"
-AARCH_ARCHIVE_NAME_JRE="sapmachine-jre-${VERSION}_${OS_NAME}-aarch64_bin.tar.gz"
-AARCH_ARCHIVE_NAME_SYMBOLS="sapmachine-jdk-${VERSION}_${OS_NAME}-aarch64_bin-symbols.tar.gz"
-AARCH_DMG_NAME_JDK="sapmachine-jdk-${VERSION}_${OS_NAME}-aarch64_bin.dmg"
-AARCH_DMG_NAME_JRE="sapmachine-jre-${VERSION}_${OS_NAME}-aarch64_bin.dmg"
+JDK_TGZ_NAME="sapmachine-jdk-${VERSION}_${OS_NAME}-${PUBLISH_PLATFORM}_bin.tar.gz"
+JDK_DMG_NAME="sapmachine-jdk-${VERSION}_${OS_NAME}-${PUBLISH_PLATFORM}_bin.dmg"
+JRE_TGZ_NAME="sapmachine-jre-${VERSION}_${OS_NAME}-${PUBLISH_PLATFORM}_bin.tar.gz"
+JRE_DMG_NAME="sapmachine-jre-${VERSION}_${OS_NAME}-${PUBLISH_PLATFORM}_bin.dmg"
+SYMBOLS_TGZ_NAME="sapmachine-jdk-${VERSION}_${OS_NAME}-${PUBLISH_PLATFORM}_bin-symbols.tar.gz"
 
-mv INTEL_JDK_TGZ $INTEL_ARCHIVE_NAME_JDK
-mv INTEL_JRE_TGZ $INTEL_ARCHIVE_NAME_JRE
-mv INTEL_SYMBOLS $INTEL_ARCHIVE_NAME_SYMBOLS
-mv INTEL_JDK_DMG $INTEL_DMG_NAME_JDK
-mv INTEL_JRE_DMG $INTEL_DMG_NAME_JRE
-mv AARCH_JDK_TGZ $AARCH_ARCHIVE_NAME_JDK
-mv AARCH_JRE_TGZ $AARCH_ARCHIVE_NAME_JRE
-mv AARCH_SYMBOLS $AARCH_ARCHIVE_NAME_SYMBOLS
-mv AARCH_JDK_DMG $AARCH_DMG_NAME_JDK
-mv AARCH_JRE_DMG $AARCH_DMG_NAME_JRE
+JDK_TGZ_SUM="$(echo $JDK_TGZ_NAME | sed 's/tar\.gz/sha256\.txt/')"
+JDK_DMG_SUM="$(echo $JDK_DMG_NAME | sed 's/dmg/dmg\.sha256\.txt/')"
+JRE_TGZ_SUM="$(echo $JRE_TGZ_NAME | sed 's/tar\.gz/sha256\.txt/')"
+JRE_DMG_SUM="$(echo $JRE_DMG_NAME | sed 's/dmg/dmg\.sha256\.txt/')"
+SYMBOLS_TGZ_SUM="$(echo $SYMBOLS_TGZ_NAME | sed 's/tar\.gz/sha256\.txt/')"
 
-INTEL_ARCHIVE_SUM_JDK="$(echo $INTEL_ARCHIVE_NAME_JDK | sed 's/tar\.gz/sha256\.txt/')"
-INTEL_ARCHIVE_SUM_JRE="$(echo $INTEL_ARCHIVE_NAME_JRE | sed 's/tar\.gz/sha256\.txt/')"
-INTEL_ARCHIVE_SUM_SYMBOLS="$(echo $INTEL_ARCHIVE_NAME_SYMBOLS | sed 's/tar\.gz/sha256\.txt/')"
-INTEL_DMG_SUM_JDK="$(echo $INTEL_DMG_NAME_JDK | sed 's/dmg/dmg\.sha256\.txt/')"
-INTEL_DMG_SUM_JRE="$(echo $INTEL_DMG_NAME_JRE | sed 's/dmg/dmg\.sha256\.txt/')"
-AARCH_ARCHIVE_SUM_JDK="$(echo $AARCH_ARCHIVE_NAME_JDK | sed 's/tar\.gz/sha256\.txt/')"
-AARCH_ARCHIVE_SUM_JRE="$(echo $AARCH_ARCHIVE_NAME_JRE | sed 's/tar\.gz/sha256\.txt/')"
-AARCH_ARCHIVE_SUM_SYMBOLS="$(echo $AARCH_ARCHIVE_NAME_SYMBOLS | sed 's/tar\.gz/sha256\.txt/')"
-AARCH_DMG_SUM_JDK="$(echo $AARCH_DMG_NAME_JDK | sed 's/dmg/dmg\.sha256\.txt/')"
-AARCH_DMG_SUM_JRE="$(echo $AARCH_DMG_NAME_JRE | sed 's/dmg/dmg\.sha256\.txt/')"
+shasum -a 256 $JDK_TGZ_NAME > $JDK_TGZ_SUM
+shasum -a 256 $JRE_TGZ_NAME > $JRE_TGZ_SUM
+shasum -a 256 $SYMBOLS_TGZ_NAME > $SYMBOLS_TGZ_SUM
+shasum -a 256 $JDK_DMG_NAME > $JDK_DMG_SUM
+shasum -a 256 $JRE_DMG_NAME > $JRE_DMG_SUM
 
-shasum -a 256 $INTEL_ARCHIVE_NAME_JDK > $INTEL_ARCHIVE_SUM_JDK
-shasum -a 256 $INTEL_ARCHIVE_NAME_JRE > $INTEL_ARCHIVE_SUM_JRE
-shasum -a 256 $INTEL_ARCHIVE_NAME_SYMBOLS > $INTEL_ARCHIVE_SUM_SYMBOLS
-shasum -a 256 $INTEL_DMG_NAME_JDK > $INTEL_DMG_SUM_JDK
-shasum -a 256 $INTEL_DMG_NAME_JRE > $INTEL_DMG_SUM_JRE
-shasum -a 256 $AARCH_ARCHIVE_NAME_JDK > $AARCH_ARCHIVE_SUM_JDK
-shasum -a 256 $AARCH_ARCHIVE_NAME_JRE > $AARCH_ARCHIVE_SUM_JRE
-shasum -a 256 $AARCH_ARCHIVE_NAME_SYMBOLS > $AARCH_ARCHIVE_SUM_SYMBOLS
-shasum -a 256 $AARCH_DMG_NAME_JDK > $AARCH_DMG_SUM_JDK
-shasum -a 256 $AARCH_DMG_NAME_JRE > $AARCH_DMG_SUM_JRE
+#debug, delete before using productively
+SAPMACHINE_VERSION="sapmachine-0.0.0"
 
-python3 SapMachine-infrastructure/lib/github_publish.py -t $SAPMACHINE_VERSION -a "${INTEL_ARCHIVE_NAME_JDK}"
-python3 SapMachine-infrastructure/lib/github_publish.py -t $SAPMACHINE_VERSION -a "${INTEL_ARCHIVE_NAME_JRE}"
-python3 SapMachine-infrastructure/lib/github_publish.py -t $SAPMACHINE_VERSION -a "${INTEL_ARCHIVE_NAME_SYMBOLS}"
-python3 SapMachine-infrastructure/lib/github_publish.py -t $SAPMACHINE_VERSION -a "${INTEL_ARCHIVE_SUM_JDK}"
-python3 SapMachine-infrastructure/lib/github_publish.py -t $SAPMACHINE_VERSION -a "${INTEL_ARCHIVE_SUM_JRE}"
-python3 SapMachine-infrastructure/lib/github_publish.py -t $SAPMACHINE_VERSION -a "${INTEL_ARCHIVE_SUM_SYMBOLS}"
-python3 SapMachine-infrastructure/lib/github_publish.py -t $SAPMACHINE_VERSION -a "${INTEL_DMG_NAME_JDK}"
-python3 SapMachine-infrastructure/lib/github_publish.py -t $SAPMACHINE_VERSION -a "${INTEL_DMG_NAME_JRE}"
-python3 SapMachine-infrastructure/lib/github_publish.py -t $SAPMACHINE_VERSION -a "${INTEL_DMG_SUM_JDK}"
-python3 SapMachine-infrastructure/lib/github_publish.py -t $SAPMACHINE_VERSION -a "${INTEL_DMG_SUM_JRE}"
-python3 SapMachine-infrastructure/lib/github_publish.py -t $SAPMACHINE_VERSION -a "${AARCH_ARCHIVE_NAME_JDK}"
-python3 SapMachine-infrastructure/lib/github_publish.py -t $SAPMACHINE_VERSION -a "${AARCH_ARCHIVE_NAME_JRE}"
-python3 SapMachine-infrastructure/lib/github_publish.py -t $SAPMACHINE_VERSION -a "${AARCH_ARCHIVE_NAME_SYMBOLS}"
-python3 SapMachine-infrastructure/lib/github_publish.py -t $SAPMACHINE_VERSION -a "${AARCH_ARCHIVE_SUM_JDK}"
-python3 SapMachine-infrastructure/lib/github_publish.py -t $SAPMACHINE_VERSION -a "${AARCH_ARCHIVE_SUM_JRE}"
-python3 SapMachine-infrastructure/lib/github_publish.py -t $SAPMACHINE_VERSION -a "${AARCH_ARCHIVE_SUM_SYMBOLS}"
-python3 SapMachine-infrastructure/lib/github_publish.py -t $SAPMACHINE_VERSION -a "${AARCH_DMG_NAME_JDK}"
-python3 SapMachine-infrastructure/lib/github_publish.py -t $SAPMACHINE_VERSION -a "${AARCH_DMG_NAME_JRE}"
-python3 SapMachine-infrastructure/lib/github_publish.py -t $SAPMACHINE_VERSION -a "${AARCH_DMG_SUM_JDK}"
-python3 SapMachine-infrastructure/lib/github_publish.py -t $SAPMACHINE_VERSION -a "${AARCH_DMG_SUM_JRE}"
+python3 SapMachine-infrastructure/lib/github_publish.py -t $SAPMACHINE_VERSION -a "${JDK_TGZ_NAME}"
+python3 SapMachine-infrastructure/lib/github_publish.py -t $SAPMACHINE_VERSION -a "${JDK_TGZ_SUM}"
+python3 SapMachine-infrastructure/lib/github_publish.py -t $SAPMACHINE_VERSION -a "${JDK_DMG_NAME}"
+python3 SapMachine-infrastructure/lib/github_publish.py -t $SAPMACHINE_VERSION -a "${JDK_DMG_SUM}"
+python3 SapMachine-infrastructure/lib/github_publish.py -t $SAPMACHINE_VERSION -a "${JRE_TGZ_NAME}"
+python3 SapMachine-infrastructure/lib/github_publish.py -t $SAPMACHINE_VERSION -a "${JRE_TGZ_SUM}"
+python3 SapMachine-infrastructure/lib/github_publish.py -t $SAPMACHINE_VERSION -a "${JRE_DMG_NAME}"
+python3 SapMachine-infrastructure/lib/github_publish.py -t $SAPMACHINE_VERSION -a "${JRE_DMG_SUM}"
+python3 SapMachine-infrastructure/lib/github_publish.py -t $SAPMACHINE_VERSION -a "${SYMBOLS_TGZ_NAME}"
+python3 SapMachine-infrastructure/lib/github_publish.py -t $SAPMACHINE_VERSION -a "${SYMBOLS_TGZ_SUM}"
+
+#debug, delete before using productively
+exit 0
 
 if [ "$PUBLISH_CASKS" == true ]; then
     python3 SapMachine-infrastructure/lib/make_cask.py -t $SAPMACHINE_VERSION
