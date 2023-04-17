e5f399a9cedb1f66cd01dd41597cbf754be88bf4
Christoph Langer <christoph.langer@sap.com>
2023-04-17 07:56:16 +0200

    Further refinements

--- lib/git_clone.sh
+++ lib/git_clone.sh
@@ -23,13 +23,14 @@ if [ ! -z $GIT_USER ]; then
 fi
 
 "$GIT_TOOL" --version
+"$GIT_TOOL" config --global init.defaultBranch master
 (set -ex && "$GIT_TOOL" init $2)
 cd $2
 
 if GIT_TERMINAL_PROMPT=0 "$GIT_TOOL" ls-remote --tags "$1" | grep -q "refs/tags/$3"; then
   # we have a tag
   (set -ex && GIT_TERMINAL_PROMPT=0 eval "$GIT_TOOL_FOR_EVAL" $GIT_CREDENTIALS fetch $DEPTH_OPTION $1 $3 $PR_SPEC)
-  (set -ex && "$GIT_TOOL" checkout FETCH_HEAD)
+  (set -ex && "$GIT_TOOL" checkout -b ref FETCH_HEAD)
   if [ ! -z $4 ]; then
     echo "Should not happen: Try to merge $4 into tag $3"
     exit -1
