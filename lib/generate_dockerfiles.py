a8e695999cd6e73518a8c8a2a27389cda73c8034
Christoph Langer <christoph.langer@sap.com>
2023-07-12 17:12:46 +0200

    Improve SapMachine dockerfile

--- lib/generate_dockerfiles.py
+++ lib/generate_dockerfiles.py
@@ -13,21 +13,18 @@ from string import Template
 from utils import github_api_request
 from versions import SapMachineTag
 
-dockerfile_template = '''
-FROM ubuntu:22.04
+dockerfile_template = '''FROM ubuntu:22.04
 
 RUN apt-get update \\
-    && apt-get install -y --no-install-recommends ca-certificates gnupg2 \\
-    && rm -rf /var/lib/apt/lists/*
-
-RUN export GNUPGHOME="$$(mktemp -d)" \\
-    && gpg --batch --keyserver hkps://keys.openpgp.org --recv-keys CACB9FE09150307D1D22D82962754C3B3ABCFE23 \\
-    && gpg --batch --export --armor 'CACB 9FE0 9150 307D 1D22 D829 6275 4C3B 3ABC FE23' > /etc/apt/trusted.gpg.d/sapmachine.gpg.asc \\
-    && gpgconf --kill all && rm -rf "$$GNUPGHOME" \\
-    && echo "deb http://dist.sapmachine.io/debian/$$(dpkg --print-architecture)/ ./" > /etc/apt/sources.list.d/sapmachine.list \\
+    && apt-get -y --no-install-recommends install ca-certificates gnupg \\
+    && export GNUPGHOME="$(mktemp -d)" \\
+    && gpg --no-default-keyring --keyring gnupg-ring:/etc/apt/trusted.gpg.d/sapmachine.gpg --batch --keyserver hkps://keys.openpgp.org --recv-keys CACB9FE09150307D1D22D82962754C3B3ABCFE23 \\
+    && chmod 644 /etc/apt/trusted.gpg.d/sapmachine.gpg \\
+    && echo "deb http://dist.sapmachine.io/debian/$(dpkg --print-architecture)/ ./" > /etc/apt/sources.list.d/sapmachine.list \\
     && apt-get update \\
     && apt-get -y --no-install-recommends install ${version} \\
-    && rm -rf /var/lib/apt/lists/*
+    && apt-get remove -y --purge --autoremove ca-certificates gnupg \\
+    && rm -rf "$GNUPGHOME" /var/lib/apt/lists/*
 
 ENV JAVA_HOME=/usr/lib/jvm/sapmachine-${major}
 
