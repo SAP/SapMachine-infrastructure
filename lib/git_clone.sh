7277ca3d4b438bfc6ec82691950553fec2bc43d3
Christoph Langer <christoph.langer@sap.com>
2023-09-05 07:23:32 +0200

    Fix handling of git commit in git_clone.sh

--- lib/git_clone.sh
+++ lib/git_clone.sh
@@ -18,16 +18,8 @@ fi
 (set -ex && "$GIT_TOOL" init $2)
 cd $2
 
-# handle tag
-if GIT_TERMINAL_PROMPT=0 eval "$GIT_TOOL_FOR_EVAL" $GIT_CREDENTIALS ls-remote --tags "$1" | grep -q "refs/tags/$3$"; then
-  if [ ! -z $4 ]; then
-    echo "Should not happen: Try to merge $4 into tag($3)"
-    exit -1
-  fi
-  (set -ex && GIT_TERMINAL_PROMPT=0 eval "$GIT_TOOL_FOR_EVAL" $GIT_CREDENTIALS fetch --depth 1 $1 $3)
-  (set -ex && "$GIT_TOOL" checkout --detach FETCH_HEAD)
 # handle branch
-elif GIT_TERMINAL_PROMPT=0 eval "$GIT_TOOL_FOR_EVAL" $GIT_CREDENTIALS ls-remote --heads "$1" | grep -q "refs/heads/$3"; then
+if GIT_TERMINAL_PROMPT=0 eval "$GIT_TOOL_FOR_EVAL" $GIT_CREDENTIALS ls-remote --heads "$1" | grep -q "refs/heads/$3"; then
   if [ ! -z $4 ]; then
     (set -ex && GIT_TERMINAL_PROMPT=0 eval "$GIT_TOOL_FOR_EVAL" $GIT_CREDENTIALS fetch --no-tags $1 $3:ref pull/$4/head:pr)
     (set -ex && "$GIT_TOOL" checkout ref)
@@ -38,7 +30,12 @@ elif GIT_TERMINAL_PROMPT=0 eval "$GIT_TOOL_FOR_EVAL" $GIT_CREDENTIALS ls-remote 
     (set -ex && GIT_TERMINAL_PROMPT=0 eval "$GIT_TOOL_FOR_EVAL" $GIT_CREDENTIALS fetch --no-tags --depth 1 $1 $3)
     (set -ex && "$GIT_TOOL" checkout --detach FETCH_HEAD)
   fi
+# handle tag or commit
 else
-  echo "$3 is not a valid git reference"
-  exit -1
+  if [ ! -z $4 ]; then
+    echo "Should not happen: Try to merge $4 into tag/commit ($3)"
+    exit -1
+  fi
+  (set -ex && GIT_TERMINAL_PROMPT=0 eval "$GIT_TOOL_FOR_EVAL" $GIT_CREDENTIALS fetch --depth 1 $1 $3)
+  (set -ex && "$GIT_TOOL" checkout --detach FETCH_HEAD)
 fi
