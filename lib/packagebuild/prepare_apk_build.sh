d6c8c9f286092c6baca6ba239ebf94e0379ae866
Christoph Langer <christoph.langer@sap.com>
2023-11-24 12:26:08 +0100

    Cleanups (mainly for apk build)

--- lib/packagebuild/prepare_apk_build.sh
+++ lib/packagebuild/prepare_apk_build.sh
@@ -7,6 +7,3 @@ cp $apkrsapub ~/.abuild/sapmachine-apk.rsa.pub
 chmod 644 ~/.abuild/sapmachine-apk.rsa.pub
 sudo bash -c "cp $HOME/.abuild/sapmachine-apk.rsa.pub /etc/apk/keys/sapmachine-apk.rsa.pub"
 echo -e PACKAGER_PRIVKEY="$HOME/.abuild/sapmachine-apk.rsa\n" > ~/.abuild/abuild.conf
-ls -la ~/.abuild
-cat ~/.abuild/abuild.conf
-ls -la /etc/apk/keys
