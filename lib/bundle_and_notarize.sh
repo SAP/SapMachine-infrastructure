b67ed23a8de367fd7cbd07d6cf2d1f29057e152e
Christoph Langer <christoph.langer@sap.com>
2024-01-13 18:10:30 +0100

    Further improvements for notarization step

--- lib/bundle_and_notarize.sh
+++ lib/bundle_and_notarize.sh
@@ -1,38 +1,29 @@
 #!/bin/bash
 set -ex
 
-# sends a notarization request and prints info and log for that request
+# Send notarization request and wait for completion. If unsuccessful, print request log and exit with error.
+KEYCHAIN_PROFILE=sapmachine-notarization
 notarize() {
-  rm noutf | true
-  xcrun notarytool submit $2 --keychain-profile "$KEYCHAIN_PROFILE" --output-format=json --wait "$1" | tee noutf
-  rc=${PIPESTATUS[0]}
-  notaryout=$(cat noutf)
-  rm noutf
+  notaryout=$(xcrun notarytool submit $2 --keychain-profile "$KEYCHAIN_PROFILE" --output-format=json --wait "$1")
+  rc=$?
   echo $notaryout
   id=$(echo "$notaryout" | grep -o '"id":"[^"]*"' | cut -d'"' -f4)
   status=$(echo "$notaryout" | grep -o '"status":"[^"]*"' | cut -d'"' -f4)
-  echo "notarytool: submitting $1 resulted in $status (id=$id)"
   if [[ $rc -eq 0 && $status == "Accepted" ]]; then
-    echo "Notarization successful."
-    return 0
+    echo "Notarization result for $1: $status (rc=$rc, id=$id)"
   else
-    echo "Notarization unsuccessful. Printing Log."
+    echo "Notarization of $1 failed: $status (rc=$rc, id=$id). Printing Log and exiting."
     xcrun notarytool log --keychain-profile "$KEYCHAIN_PROFILE" --output-format=json $id
-    return 1
+    exit 1
   fi
 }
-
-KEYCHAIN_PROFILE=sapmachine-notarization
 
 if [[ -z $WORKSPACE ]]; then
   WORKSPACE=$PWD
 fi
 
-cd "${WORKSPACE}/SapMachine"
-
-cd build
-cd "$(ls)"
-cd bundles
+cd "${WORKSPACE}/SapMachine/build"
+cd "$(ls)/bundles"
 
 JDK_NAME=$(ls *jdk-*_bin.*) || true
 if [ -z $JDK_NAME ]; then
@@ -64,10 +55,7 @@ DMG_NOTARIZE_BASE="${WORKSPACE}/dmg_notarize_base"
 security unlock-keychain -p $unlockpass ~/Library/Keychains/login.keychain
 
 # JDK
-if notarize "${WORKSPACE}/${ARCHIVE_NAME_JDK}" "--force"; then
-  return 1
-fi
-
+notarize "${WORKSPACE}/${ARCHIVE_NAME_JDK}" "--force"
 DMG_NAME_JDK=$(basename ${ARCHIVE_NAME_JDK} .tar.gz)
 rm -rf ${DMG_NOTARIZE_BASE}
 mkdir -p ${DMG_NOTARIZE_BASE}
@@ -75,27 +63,19 @@ tar -xzf "${WORKSPACE}/${ARCHIVE_NAME_JDK}" -C ${DMG_NOTARIZE_BASE}
 hdierror=0
 hdiutil create -srcfolder ${DMG_NOTARIZE_BASE} -fs HFS+ -volname ${DMG_NAME_JDK} "${WORKSPACE}/${DMG_NAME_JDK}.dmg" || hdierror=1
 if [ $hdierror -ne 0 ]; then
-  # We see sometimes errors like "hdiutil: create failed - Resource busy." when invoking it right after tar.
-  # Let's retry after sleeping a little while.
+  # Sometimes we see errors like "hdiutil: create failed - Resource busy." Let's retry once after sleeping a little while.
   sleep 30
   hdiutil create -verbose -srcfolder ${DMG_NOTARIZE_BASE} -fs HFS+ -volname ${DMG_NAME_JDK} "${WORKSPACE}/${DMG_NAME_JDK}.dmg"
 fi
 echo "${DMG_NAME_JDK}.dmg" > "${WORKSPACE}/jdk_dmg_name.txt"
-
 xcrun stapler staple ${DMG_NOTARIZE_BASE}/*
 rm "${WORKSPACE}/${ARCHIVE_NAME_JDK}"
 tar -czf "${WORKSPACE}/${ARCHIVE_NAME_JDK}" -C ${DMG_NOTARIZE_BASE} .
-if notarize "${WORKSPACE}/${DMG_NAME_JDK}.dmg"; then
-  return 1
-fi
-
+notarize "${WORKSPACE}/${DMG_NAME_JDK}.dmg"
 xcrun stapler staple "${WORKSPACE}/${DMG_NAME_JDK}.dmg"
 
 # JRE
-if notarize "${WORKSPACE}/${ARCHIVE_NAME_JRE}" "--force"; then
-  return 1
-fi
-
+notarize "${WORKSPACE}/${ARCHIVE_NAME_JRE}" "--force"
 DMG_NAME_JRE=$(basename ${ARCHIVE_NAME_JRE} .tar.gz)
 rm -rf ${DMG_NOTARIZE_BASE}
 mkdir -p ${DMG_NOTARIZE_BASE}
@@ -103,18 +83,13 @@ tar -xzf "${WORKSPACE}/${ARCHIVE_NAME_JRE}" -C ${DMG_NOTARIZE_BASE}
 hdierror=0
 hdiutil create -srcfolder ${DMG_NOTARIZE_BASE} -fs HFS+ -volname ${DMG_NAME_JRE} "${WORKSPACE}/${DMG_NAME_JRE}.dmg" || hdierror=1
 if [ $hdierror -ne 0 ]; then
-  # We see sometimes errors like "hdiutil: create failed - Resource busy." when invoking it right after tar.
-  # Let's retry after sleeping a little while.
+  # Sometimes we see errors like "hdiutil: create failed - Resource busy." Let's retry once after sleeping a little while.
   sleep 30
   hdiutil create -verbose -srcfolder ${DMG_NOTARIZE_BASE} -fs HFS+ -volname ${DMG_NAME_JRE} "${WORKSPACE}/${DMG_NAME_JRE}.dmg"
 fi
 echo "${DMG_NAME_JRE}.dmg" > "${WORKSPACE}/jre_dmg_name.txt"
-
-# Notarize if doing a release build
 xcrun stapler staple ${DMG_NOTARIZE_BASE}/*
 rm "${WORKSPACE}/${ARCHIVE_NAME_JRE}"
 tar -czf "${WORKSPACE}/${ARCHIVE_NAME_JRE}" -C ${DMG_NOTARIZE_BASE} .
-if notarize "${WORKSPACE}/${DMG_NAME_JRE}.dmg"; then
-  return 1
-fi
+notarize "${WORKSPACE}/${DMG_NAME_JRE}.dmg"
 xcrun stapler staple "${WORKSPACE}/${DMG_NAME_JRE}.dmg"
