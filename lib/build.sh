68ab0e69f1e1a426418d1c7616cc7cc2e899e6af
Christoph Langer <christoph.langer@sap.com>
2024-01-15 12:05:25 +0100

    Fixes after latest test

--- lib/build.sh
+++ lib/build.sh
@@ -77,4 +77,8 @@ $_CONFIGURE_OS_OPTIONS \
 --with-freetype=bundled \
 $EXTRA_CONFIGURE_OPTIONS)
 
-(set -x && make JOBS=12 product-bundles legacy-bundles test-image)
+if [ "$RELEASE_BUILD" != true ]; then
+  _TEST_IMAGE_OPT=" test-image"
+fi
+
+(set -x && make JOBS=12 product-bundles legacy-bundles$_TEST_IMAGE_OPT)
