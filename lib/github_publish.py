963ed3c4e53812dd15760037185ac2c868c2689c
Christoph Langer <christoph.langer@sap.com>
2024-03-08 11:41:16 +0100

    Refactor code to publish on GitHub. Detect the GitHub API URL from the repository and be prepared to accept a different setting

--- lib/github_publish.py
+++ lib/github_publish.py
@@ -22,41 +22,26 @@ def main(argv=None):
     parser.add_argument('-g', '--github', default='https://api.github.com', help='the github api url to be used', metavar='GITHUB', required=False)
     args = parser.parse_args()
 
-    tag = args.tag
-    asset = args.asset
-
-    if asset is not None:
-        asset_file = os.path.realpath(asset)
-        asset_name = os.path.basename(asset_file)
-        asset_mime_type = mimetypes.guess_type(asset_file)
-
-        if asset_mime_type is None or asset_mime_type[0] is None:
-            asset_mime_type = 'application/octet-stream'
-            print(str.format('Could not detect mime-type, falling back to: "{0}"', asset_mime_type))
-        else:
-            asset_mime_type = asset_mime_type[0]
-            print(str.format('Detected mime-type: "{0}"', asset_mime_type))
-
     try:
-        release = utils.github_api_request(f"releases/tags/{tag}", github_api_url=args.github)
+        release = utils.github_api_request(f"releases/tags/{args.tag}", github_api_url=args.github)
         release_id = release['id']
         upload_url = release['upload_url']
     except HTTPError as httpError:
-        print(f"Release {tag} does not seem to exist: {httpError.code} ({httpError.reason})")
+        print(f"Release {args.tag} does not seem to exist: {httpError.code} ({httpError.reason})")
         release_id = upload_url = None
 
     if release_id is None:
         # release does not exist yet -> create it
-        data = json.dumps({ "tag_name": tag, "name": tag, "body": args.description, "draft": False, "prerelease": args.prerelease })
+        data = json.dumps({ "tag_name": args.tag, "name": args.tag, "body": args.description, "draft": False, "prerelease": args.prerelease })
         try:
             response = utils.github_api_request('releases', github_api_url=args.github, data=data, method='POST', content_type='application/json')
             release_id = response['id']
             upload_url = response['upload_url']
-            print(f"Created release \"{tag}\"")
+            print(f"Created release \"{args.tag}\"")
         except HTTPError as http_err:
-            print(f"Error creating release \"{tag}\". Maybe it exists now, check...")
+            print(f"Error creating release \"{args.tag}\". Maybe it exists now, check...")
             try:
-                release = utils.github_api_request(f"releases/tags/{tag}", github_api_url=args.github)
+                release = utils.github_api_request(f"releases/tags/{args.tag}", github_api_url=args.github)
                 release_id = release['id']
                 upload_url = release['upload_url']
                 print(f"Yes, release id: {release_id}")
@@ -64,14 +49,24 @@ def main(argv=None):
                 print(f"Nope, must be something else: {httpError.code} ({httpError.reason})")
                 return 1
 
-    if asset is not None:
+    if args.asset is not None:
         # asset file is specified (-a)
+        asset_file = os.path.realpath(args.asset)
+        asset_name = os.path.basename(asset_file)
+        asset_mime_type = mimetypes.guess_type(asset_file)
+
+        if asset_mime_type is None or asset_mime_type[0] is None:
+            asset_mime_type = 'application/octet-stream'
+            print(f'Could not detect mime-type of {args.asset}, falling back to: {asset_mime_type}')
+        else:
+            asset_mime_type = asset_mime_type[0]
+            print(f'Detected mime-type of {args.asset}: {asset_mime_type}')
 
         # first check wether the asset already exists
         for gh_asset in utils.github_api_request(f'releases/{release_id}/assets', github_api_url=args.github, per_page=50):
             if gh_asset['name'] == asset_name:
                 # asset already exists -> skip
-                print(str.format('Error: Asset "{0}" already exists.', asset_name))
+                print(f'Error: Asset "{asset_name}" already exists.')
                 return 1
 
         upload_url = str(upload_url.split('{', 1)[0] + '?name=' + quote(asset_name))
@@ -86,13 +81,13 @@ def main(argv=None):
         while retry > 0:
             try:
                 # upload the asset file
-                print(str.format('Uploading asset "{0}" with a length of {1} bytes...', asset_name, str(asset_length)))
+                print(f'Uploading asset "{asset_name}" with a length of {str(asset_length)} bytes...')
                 utils.github_api_request(url=upload_url, data=asset_data, method='POST', content_type=asset_mime_type)
                 return 0
             except IOError as e:
                 # _type, value, _traceback = sys.exc_info()
                 # traceback.print_exception(_type, value, _traceback)
-                print(str.format('Error uploading asset "{0}": {1}', asset_name, e))
+                print(f'Error uploading asset "{asset_name}": {e}')
                 retry -= 1
         return 1
 
--- lib/publish.sh
+++ lib/publish.sh
@@ -4,54 +4,58 @@ set -e
 
 UNAME=`uname`
 
+if [[ -z "$SAPMACHINE_VERSION" ]]; then
+  echo "SAPMACHINE_VERSION not set"
+  exit 1
+fi
+
+if [[ -z $SAPMACHINE_GIT_REPOSITORY ]]; then
+  echo "SAPMACHINE_GIT_REPOSITORY not set"
+  exit 1
+fi
+
+if [[ -z $GITHUB_API_URL ]]; then
+  if [[ $SAPMACHINE_GIT_REPOSITORY == "*//github.com/*" ]]; then
+    GITHUB_API_URL="https://api.github.com"
+    echo "GitHub API URL set to $GITHUB_API_URL"
+  else
+    echo "GITHUB_API_URL not set or could not be determined from git repository URL ($SAPMACHINE_GIT_REPOSITORY)"
+    exit 1
+  fi
+else
+  echo "GitHub API URL is $GITHUB_API_URL"
+fi
+
 if [ "$RELEASE" == true ]; then
   PRE_RELEASE_OPT=""
 else
   PRE_RELEASE_OPT="-p"
 fi
 
-if [[ -z $SAPMACHINE_GIT_REPOSITORY ]]; then
-  SAPMACHINE_GIT_REPOSITORY="http://github.com/SAP/SapMachine.git"
-fi
-
-if [[ -z "$SAPMACHINE_VERSION" ]]; then
-  echo "SAPMACHINE_VERSION not set"
-  exit 1
-fi
-python3 SapMachine-infrastructure/lib/github_publish.py -t $SAPMACHINE_VERSION $PRE_RELEASE_OPT || true
-
-ls -la
+python3 SapMachine-infrastructure/lib/github_publish.py -t $SAPMACHINE_VERSION -g $GITHUB_API_URL $PRE_RELEASE_OPT
 
 ARCHIVE_NAME_JDK="$(cat jdk_bundle_name.txt)"
 ARCHIVE_NAME_JRE="$(cat jre_bundle_name.txt)"
 ARCHIVE_NAME_SYMBOLS="$(cat symbols_bundle_name.txt)"
-
-HAS_ZIP=$(ls sapmachine-jdk-*_bin.zip | wc -l)
-
-if [ "$HAS_ZIP" -lt "1" ]; then
-  ARCHIVE_SUM_JDK="$(echo $ARCHIVE_NAME_JDK | sed 's/tar\.gz/sha256\.txt/')"
-  ARCHIVE_SUM_JRE="$(echo $ARCHIVE_NAME_JRE | sed 's/tar\.gz/sha256\.txt/')"
-else
-  ARCHIVE_SUM_JDK="$(echo $ARCHIVE_NAME_JDK | sed 's/zip/sha256\.txt/')"
-  ARCHIVE_SUM_JRE="$(echo $ARCHIVE_NAME_JRE | sed 's/zip/sha256\.txt/')"
-fi
+ARCHIVE_SUM_JDK="$(echo $ARCHIVE_NAME_JDK | sed 's/tar\.gz\|zip/sha256\.txt/')"
+ARCHIVE_SUM_JRE="$(echo $ARCHIVE_NAME_JRE | sed 's/tar\.gz\|zip/sha256\.txt/')"
 ARCHIVE_SUM_SYMBOLS="$(echo $ARCHIVE_NAME_SYMBOLS | sed 's/tar\.gz/sha256\.txt/')"
 
 shasum -a 256 $ARCHIVE_NAME_JDK > $ARCHIVE_SUM_JDK
 shasum -a 256 $ARCHIVE_NAME_JRE > $ARCHIVE_SUM_JRE
 shasum -a 256 $ARCHIVE_NAME_SYMBOLS > $ARCHIVE_SUM_SYMBOLS
 
-python3 SapMachine-infrastructure/lib/github_publish.py -t $SAPMACHINE_VERSION -a "${ARCHIVE_NAME_JDK}"
-python3 SapMachine-infrastructure/lib/github_publish.py -t $SAPMACHINE_VERSION -a "${ARCHIVE_NAME_JRE}"
-python3 SapMachine-infrastructure/lib/github_publish.py -t $SAPMACHINE_VERSION -a "${ARCHIVE_NAME_SYMBOLS}"
-python3 SapMachine-infrastructure/lib/github_publish.py -t $SAPMACHINE_VERSION -a "${ARCHIVE_SUM_JDK}"
-python3 SapMachine-infrastructure/lib/github_publish.py -t $SAPMACHINE_VERSION -a "${ARCHIVE_SUM_JRE}"
-python3 SapMachine-infrastructure/lib/github_publish.py -t $SAPMACHINE_VERSION -a "${ARCHIVE_SUM_SYMBOLS}"
+python3 SapMachine-infrastructure/lib/github_publish.py -t $SAPMACHINE_VERSION -g $GITHUB_API_URL -a "${ARCHIVE_NAME_JDK}"
+python3 SapMachine-infrastructure/lib/github_publish.py -t $SAPMACHINE_VERSION -g $GITHUB_API_URL -a "${ARCHIVE_NAME_JRE}"
+python3 SapMachine-infrastructure/lib/github_publish.py -t $SAPMACHINE_VERSION -g $GITHUB_API_URL -a "${ARCHIVE_NAME_SYMBOLS}"
+python3 SapMachine-infrastructure/lib/github_publish.py -t $SAPMACHINE_VERSION -g $GITHUB_API_URL -a "${ARCHIVE_SUM_JDK}"
+python3 SapMachine-infrastructure/lib/github_publish.py -t $SAPMACHINE_VERSION -g $GITHUB_API_URL -a "${ARCHIVE_SUM_JRE}"
+python3 SapMachine-infrastructure/lib/github_publish.py -t $SAPMACHINE_VERSION -g $GITHUB_API_URL -a "${ARCHIVE_SUM_SYMBOLS}"
 
 if [[ $UNAME == "Linux" ]] && [ "$RELEASE" == true ]; then
   for RPMFILE in *.rpm; do
     [ -f "$RPMFILE" ] || continue
-    python3 SapMachine-infrastructure/lib/github_publish.py -t ${SAPMACHINE_VERSION} -a ${RPMFILE}
+    python3 SapMachine-infrastructure/lib/github_publish.py -t ${SAPMACHINE_VERSION} -g $GITHUB_API_URL -a ${RPMFILE}
   done
 fi
 
@@ -65,10 +69,10 @@ if [ $UNAME == Darwin ]; then
   shasum -a 256 $DMG_NAME_JDK > $DMG_SUM_JDK
   shasum -a 256 $DMG_NAME_JRE > $DMG_SUM_JRE
 
-  python3 SapMachine-infrastructure/lib/github_publish.py -t $SAPMACHINE_VERSION -a "${DMG_NAME_JDK}"
-  python3 SapMachine-infrastructure/lib/github_publish.py -t $SAPMACHINE_VERSION -a "${DMG_NAME_JRE}"
-  python3 SapMachine-infrastructure/lib/github_publish.py -t $SAPMACHINE_VERSION -a "${DMG_SUM_JDK}"
-  python3 SapMachine-infrastructure/lib/github_publish.py -t $SAPMACHINE_VERSION -a "${DMG_SUM_JRE}"
+  python3 SapMachine-infrastructure/lib/github_publish.py -t $SAPMACHINE_VERSION -g $GITHUB_API_URL -a "${DMG_NAME_JDK}"
+  python3 SapMachine-infrastructure/lib/github_publish.py -t $SAPMACHINE_VERSION -g $GITHUB_API_URL -a "${DMG_NAME_JRE}"
+  python3 SapMachine-infrastructure/lib/github_publish.py -t $SAPMACHINE_VERSION -g $GITHUB_API_URL -a "${DMG_SUM_JDK}"
+  python3 SapMachine-infrastructure/lib/github_publish.py -t $SAPMACHINE_VERSION -g $GITHUB_API_URL -a "${DMG_SUM_JRE}"
 fi
 
 #if [[ $UNAME == CYGWIN* ]]; then
