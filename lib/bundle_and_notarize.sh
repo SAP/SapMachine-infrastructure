bd375d4a92ea93f95001fdfef120a17353515024
Christoph Langer <christoph.langer@sap.com>
2024-06-07 08:04:48 +0200

    Use a separate keychain for notarytool, default keychain makes problems on interactive jenkins clients

--- lib/bundle_and_notarize.sh
+++ lib/bundle_and_notarize.sh
@@ -5,7 +5,7 @@ set -e
 # Send notarization request and wait for completion. If unsuccessful, print request log and exit with error.
 KEYCHAIN_PROFILE=sapmachine-notarization
 notarize() {
-  notaryout=$(set -x && xcrun notarytool submit $2 --keychain-profile "$KEYCHAIN_PROFILE" --output-format=json --wait "$1")
+  notaryout=$(set -x && xcrun notarytool submit $2 --keychain ~/jenkins.keychain --keychain-profile "$KEYCHAIN_PROFILE" --output-format=json --wait "$1")
   rc=$?
   echo $notaryout
   id=$(echo "$notaryout" | grep -o '"id":"[^"]*"' | cut -d'"' -f4)
@@ -14,7 +14,7 @@ notarize() {
     echo "Notarization result for $1: $status (rc=$rc, id=$id)"
   else
     echo "Notarization of $1 failed: $status (rc=$rc, id=$id). Printing Log and exiting."
-    xcrun notarytool log --keychain-profile "$KEYCHAIN_PROFILE" --output-format=json $id
+    xcrun notarytool log --keychain ~/jenkins.keychain --keychain-profile "$KEYCHAIN_PROFILE" --output-format=json $id
     exit 1
   fi
 }
@@ -50,7 +50,7 @@ fi
 # Prepare
 rm -rf *.dmg
 DMG_NOTARIZE_BASE="${WORKSPACE}/dmg_notarize_base"
-security unlock-keychain -p $unlockpass ~/Library/Keychains/login.keychain
+security unlock-keychain -p changeit ~/jenkins.keychain
 
 # JDK
 notarize "${WORKSPACE}/${ARCHIVE_NAME_JDK}" "--force"
