51e754ea6ceceee9cd680c88d5f1d276cfd6d44d
Christoph Langer <christoph.langer@sap.com>
2024-06-13 17:18:48 +0200

    More migrated jobs

--- lib/packagebuild/prepare_apk_build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-mkdir ~/.abuild
-cp $apkrsa ~/.abuild/sapmachine-apk.rsa
-chmod 600 ~/.abuild/sapmachine-apk.rsa
-cp $apkrsapub ~/.abuild/sapmachine-apk.rsa.pub
-chmod 644 ~/.abuild/sapmachine-apk.rsa.pub
-sudo bash -c "cp $HOME/.abuild/sapmachine-apk.rsa.pub /etc/apk/keys/sapmachine-apk.rsa.pub"
-echo -e PACKAGER_PRIVKEY="$HOME/.abuild/sapmachine-apk.rsa\n" > ~/.abuild/abuild.conf
